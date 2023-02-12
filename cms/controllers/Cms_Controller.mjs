f679fd110ae41eddad536246e32039bd6f7ef2f0
Nicholas Alvén <nicholas@nikolaidev.com>
2023-02-12 21:26:00 +0100

    started copying over files from frontendFramework

---
+++ cms/controllers/Cms_Controller.mjs
@@ -0,0 +1,18 @@
+// import {View} from "nd_frontend/core/View.mjs";
+// import {Template_Start_View} from "nd_frontend/generics/components/templates/Template_Start_View.mjs";
+// import {Start_Model} from "../models/Start_Model.mjs";
+
+// export function Cms_Controller(){
+
+//     View.call(this)
+
+//     this.title = "Start"
+//     const view = "start"
+
+//     // model
+//     Start_Model(view)
+
+//     // view
+//     this.template = new Template_Start_View(view)
+
+// }
