91428410504851ed9f93b778fa15ae2200b5ebb4
Maja Frankiewicz <majafrank@live.se>
2023-02-12 23:16:30 +0100

    fix: more cms

--- cms/controllers/Cms_Controller.mjs
+++ cms/controllers/Cms_Controller.mjs
@@ -1,15 +1,24 @@
 import {Controller} from "../../core/Controller.mjs";
 import {fetchModel} from "../functions/fetchModel.mjs";
 import {Architect} from "../functions/Architect.mjs";
+import { View } from "../../core/View.mjs";
+import { t_Split } from "../../components/templates/t_Split.mjs";
 
 export async function Cms_Controller() {
+
+    View.class(this)
+
+    this.title = "CMS"
+    const view = "cms"
+
     let schema = await fetchModel()
     console.log(schema)
 
+    this.template = new t_Split(view);
     // Controller.call(this)
     // this.schema = schema
-    let archi = new Architect()
-    document.body.insertAdjacentElement("afterbegin", archi.getPanel())
+    // let archi = new Architect()
+    // document.body.insertAdjacentElement("afterbegin", archi.getPanel())
 
 
 }
---
+++ core/View.mjs
@@ -0,0 +1,21 @@
+
+export function View() {
+
+    // As it is a constructor, View itself cannot be async, Async functions needed for a view should be wrapped into methods (like the setView method below).
+
+    this.title= "default title"
+    this.template= null
+    
+    /**
+     * Called by router. In most cases, should not be overridden.
+     * @returns {Promise<void>}
+     */
+    this.setView= async function() {
+
+        if(!this.template) throw new Error("View template is not set")
+        document.body.append(this.template.getElement())
+
+        document.title = this.title
+    }
+
+}
