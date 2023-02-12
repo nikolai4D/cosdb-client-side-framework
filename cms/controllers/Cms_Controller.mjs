42414e5f694e628eb64c82249500e90e2b9c9ff5
Maja Frankiewicz <majafrank@live.se>
2023-02-12 22:31:59 +0100

    fix: error

--- cms/controllers/Cms_Controller.mjs
+++ cms/controllers/Cms_Controller.mjs
@@ -2,9 +2,10 @@ import {Controller} from "../../core/Controller.mjs";
 import {fetchModel} from "../functions/fetchModel.mjs";
 import {Architect} from "../functions/Architect.mjs";
 
-let schema = fetchModel()
-console.log(schema)
-
+export function Cms_Controller() {
+    let schema = fetchModel()
+    console.log(schema)
+}
 
 // let archi = new Architect(controller)
 // document.body.insertAdjacentElement("afterbegin", archi.getPanel(controller))
