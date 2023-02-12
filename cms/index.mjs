3fd4501c73b0b1a0b51ce35b2b62012c8f1662c5
Nicholas Alvén <nicholas@nikolaidev.com>
2023-02-12 21:26:00 +0100

    started copying over files from frontendFramework

--- cms/index.mjs
+++ cms/index.mjs
@@ -1,3 +1,12 @@
-import { handleLoad } from "./functions/handleLoad.mjs";
+// import { handleLoad } from "./functions/handleLoad.mjs";
 
-window.onload = handleLoad;
+// window.onload = handleLoad;
+
+import { Router, route } from "../core/Router.mjs";
+
+export const router = new Router([route("cms", Start_Controller)]);
+
+const path = window.location.pathname.slice(1);
+console.log("path: ", path);
+
+router.goTo(path);
