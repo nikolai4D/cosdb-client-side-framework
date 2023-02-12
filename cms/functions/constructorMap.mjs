c3791b0bedd318c6916830a33a55a05701a3caed
Maja Frankiewicz <majafrank@live.se>
2023-02-12 22:28:11 +0100

    fix: path

--- cms/functions/constructorMap.mjs
+++ cms/functions/constructorMap.mjs
@@ -1,4 +1,4 @@
-import {t_Split} from "../generics/components/templates/t_Split.mjs";
+import {t_Split} from "../../components/templates/t_Split.mjs";
 
 export const constructorMap = new Map([
     ["t_Split", t_Split],
