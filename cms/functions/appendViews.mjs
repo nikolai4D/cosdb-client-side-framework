8e6871cc6fe67813de0edcca10f80d9ac66af1a1
Nicholas Alvén <nicholas@nikolaidev.com>
2023-02-13 18:01:52 +0100

    auto commit to dev

--- cms/functions/appendViews.mjs
+++ cms/functions/appendViews.mjs
@@ -5,18 +5,55 @@ import { existingViews } from "./existingViews.mjs";
 export async function appendViews() {
   const data = await readModel();
 
-  const views = data.views;
+  // Define a function to create a nested set of div elements
+  function createDivs(obj, indent = 0) {
+    // Create a div for this object
+    const div = document.createElement("div");
 
-  const keys = Object.keys(views);
+    // Indent the div using CSS
+    div.style.marginLeft = `${indent}em`;
 
-  const body = document.querySelector("body");
+    // If the object has a "name" property, create a label and input
+    if (obj.hasOwnProperty("name")) {
+      const label = document.createElement("label");
+      label.textContent = obj.name;
+      div.appendChild(label);
 
-  const createNewContainer = document.createElement("div");
-  body.appendChild(createNewContainer);
+      const input = document.createElement("input");
+      input.type = "text";
+      div.appendChild(input);
+    }
 
-  const existingViewsContainer = document.createElement("div");
-  body.appendChild(existingViewsContainer);
+    // Recursively create divs for any child objects
+    for (const key in obj) {
+      if (typeof obj[key] === "object") {
+        const childDiv = createDivs(obj[key], indent + 1);
+        div.appendChild(childDiv);
+      }
+    }
 
-  createNew(existingViewsContainer, createNewContainer, keys);
-  existingViews(existingViewsContainer, views);
+    // Return the div
+    return div;
+  }
+
+  // Call the createDivs function with the top-level object in the data
+  const contentContainer = document.querySelector("#content-container");
+  const topLevelObject = data.views[0];
+  const topLevelDiv = createDivs(topLevelObject);
+  contentContainer.appendChild(topLevelDiv);
+
+  //   const views = data.views;
+
+  //   const keys = Object.keys(views);
+
+  //   const body = document.querySelector("body");
+
+  //   const createNewContainer = document.createElement("div");
+  //   body.appendChild(createNewContainer);
+
+  //   const existingViewsContainer = document.createElement("div");
+  //   body.appendChild(existingViewsContainer);
+
+  //   createNew(existingViewsContainer, createNewContainer, keys);
+  //   existingViews(existingViewsContainer, views);
 }
