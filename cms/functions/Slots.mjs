cf398433f944f58a1bcb8626333f69fc981180e7
Nicholas Alvén <nicholas@nikolaidev.com>
2023-02-13 23:34:36 +0100

    auto commit to dev

--- cms/functions/Slots.mjs
+++ cms/functions/Slots.mjs
@@ -2,13 +2,16 @@ import { accordian } from "./types/accordian.mjs";
 import { dropdown } from "./types/dropdown.mjs";
 
 const organisms = ["organism1", "organism2", "organism3"];
+const functions = ["function1", "function2", "function3"];
 
 export function Slots(slots) {
   let slotsHtml = "";
 
   for (const slot of slots) {
-    const organism = `organism: ${dropdown(organisms, slot.organism)}`;
-    slotsHtml += `${accordian("slot", slot.slot, organism, true)} <br>`;
+    const body = `organism: ${dropdown(organisms, slot.organism)}<br>
+    functions: FUNCTIONS<br>
+    molecules: MOLECULES`;
+    slotsHtml += `${accordian("slot", slot.slot, body, true)} <br>`;
   }
 
   return `   
