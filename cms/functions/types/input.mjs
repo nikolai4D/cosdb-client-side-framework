332be7b23b4563d9f67e338d0dd8a8f6ecc645e8
Nicholas Alvén <nicholas@nikolaidev.com>
2023-02-13 23:08:29 +0100

    auto commit to dev

--- cms/functions/types/input.mjs
+++ cms/functions/types/input.mjs
@@ -1,9 +1,9 @@
 // import { writeModel } from "./requests/writeModel.mjs";
 
-export function input(key, value, keyLock) {
+export function input(key, value, keyDisabled) {
   const input = `
       <label>${key}:</label>
-      <input value="${value}" disabled="${keyLock}">
+      <input value="${value}" disabled="${keyDisabled}">
     `;
 
   return input;
