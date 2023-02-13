7af379c97f630612f07d33fd222c44421f244eec
Nicholas Alvén <nicholas@nikolaidev.com>
2023-02-13 14:03:04 +0100

    changed to new files

--- cms/index.mjs
+++ cms/index.mjs
@@ -1,11 +1,11 @@
-// import { handleLoad } from "./functions/handleLoad.mjs";
-// window.onload = handleLoad;
-import {Cms_Controller} from "./controllers/Cms_Controller.mjs";
-import { Router, route } from "../core/Router.mjs";
+import { handleLoad } from "./functions/handleLoad.mjs";
+window.onload = handleLoad;
+// import {Cms_Controller} from "./controllers/Cms_Controller.mjs";
+// import { Router, route } from "../core/Router.mjs";
 
-export const router = new Router([route("cms", Cms_Controller)]);
+// export const router = new Router([route("cms", Cms_Controller)]);
 
-const path = window.location.pathname.slice(1);
-console.log("path: ", path);
+// const path = window.location.pathname.slice(1);
+// console.log("path: ", path);
 
-router.goTo(path);
+// router.goTo(path);
--- core/helpers.mjs
+++ core/helpers.mjs
@@ -1,4 +1,4 @@
-import {constructorMap} from "../cms/functions/constructorMap.mjs";
+import { constructorMap } from "../cms/functions/fromFrontendFramework/constructorMap.mjs";
 
 /**
  * Take a string and make it into an HTML element.
@@ -6,67 +6,79 @@ import {constructorMap} from "../cms/functions/constructorMap.mjs";
  * @param {string} string
  * @returns {Element}
  */
-export function stringToHTMLElement (string) {
+export function stringToHTMLElement(string) {
+  const frame = document.createElement("div");
 
-    const frame = document.createElement("div");
+  frame.insertAdjacentHTML("afterbegin", string);
 
-    frame.insertAdjacentHTML("afterbegin", string);
-
-    return frame.firstElementChild
+  return frame.firstElementChild;
 }
 
+export function buildComponentFromSchema(
+  componentSchema,
+  parentSchema = null,
+  name = "root"
+) {
+  try {
+    let constructor = constructorMap.get(componentSchema.constructorKey);
 
-export function buildComponentFromSchema(componentSchema, parentSchema = null, name = "root") {
-
-    try {
-        let constructor = constructorMap.get(componentSchema.constructorKey)
-
-        if(!constructor) {
-            console.warn(`Component type ${componentSchema.constructorKey} not found in componentsMap`)
-            return null
-        }
-
-        let component = new constructor(componentSchema.options)
-        component.options = componentSchema.options
-        component.id = name
-        componentSchema.id = name
-        for (let subComponent in componentSchema.subComponents) {
-            if(componentSchema.subComponents[subComponent]) {
-                component.subComponents[subComponent] = buildComponentFromSchema(componentSchema.subComponents[subComponent], componentSchema, name + "__" + subComponent)
-            }
-        }
-
-        return component
-
-    } catch (e) {
-        let key = (!parentSchema) ? "template" : Object.keys(parentSchema.subComponents).find(key => parentSchema.subComponents[key] === componentSchema)
-
-        console.error(`Error while creating component ${key}: ${e}`)
+    if (!constructor) {
+      console.warn(
+        `Component type ${componentSchema.constructorKey} not found in componentsMap`
+      );
+      return null;
     }
+
+    let component = new constructor(componentSchema.options);
+    component.options = componentSchema.options;
+    component.id = name;
+    componentSchema.id = name;
+    for (let subComponent in componentSchema.subComponents) {
+      if (componentSchema.subComponents[subComponent]) {
+        component.subComponents[subComponent] = buildComponentFromSchema(
+          componentSchema.subComponents[subComponent],
+          componentSchema,
+          name + "__" + subComponent
+        );
+      }
+    }
+
+    return component;
+  } catch (e) {
+    let key = !parentSchema
+      ? "template"
+      : Object.keys(parentSchema.subComponents).find(
+          (key) => parentSchema.subComponents[key] === componentSchema
+        );
+
+    console.error(`Error while creating component ${key}: ${e}`);
+  }
 }
 
 export function buildSchemaFromComponent(component) {
-
-    let subComponentsSchema = {}
-    for (let key in component.subComponents) {
-        if(!component.subComponents[key]) {
-            console.warn(`No subComponent found for key ${key} in component ${component.id}`)
-            subComponentsSchema[key] = {
-                constructorKey: null,
-                options: {},
-                subComponents: {},
-                id: component.id + "__" + key
-            }
-        }
-        else {
-            subComponentsSchema[key] = buildSchemaFromComponent(component.subComponents[key])
-        }
+  let subComponentsSchema = {};
+  for (let key in component.subComponents) {
+    if (!component.subComponents[key]) {
+      console.warn(
+        `No subComponent found for key ${key} in component ${component.id}`
+      );
+      subComponentsSchema[key] = {
+        constructorKey: null,
+        options: {},
+        subComponents: {},
+        id: component.id + "__" + key,
+      };
+    } else {
+      subComponentsSchema[key] = buildSchemaFromComponent(
+        component.subComponents[key]
+      );
     }
+  }
 
-    return {
-        "options": component.options,
-        "constructorKey": component.constructorKey,
-        "subComponents": subComponentsSchema,
-        "id": component.id,
-    }
+  return {
+    options: component.options,
+    constructorKey: component.constructorKey,
+    subComponents: subComponentsSchema,
+    id: component.id,
+  };
 }
