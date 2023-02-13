19401a01875f8925d973b93773cdd0a5c69277dd
Nicholas Alvén <nicholas@nikolaidev.com>
2023-02-13 22:15:55 +0100

    auto commit to dev

--- cms/functions/View.mjs
+++ cms/functions/View.mjs
@@ -3,12 +3,13 @@ import { dropdown } from "./types/dropdown.mjs";
 
 const viewTemplates = ["viewTemplate1","viewTemplate2","viewTemplate3"]
 
-const key = "view",
-const value = view.view,
-const body = `viewTemplate: ${dropdown(viewTemplates, view.viewTemplate)}`
-
-
 export function View(view) {
+
+    const key = "view",
+    const value = view.view,
+    const body = `viewTemplate: ${dropdown(viewTemplates, view.viewTemplate)}`
+
+
   return `   
       <div class="view">
           ${accordian(key, value, body)}
