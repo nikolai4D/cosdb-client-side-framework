72ffaa4f3a57598befaa982eea5db0f68174eb69
Nicholas Alvén <nicholas@nikolaidev.com>
2023-02-13 23:48:39 +0100

    auto commit to dev

--- cms/functions/Functions.mjs
+++ cms/functions/Functions.mjs
@@ -1,12 +1,24 @@
 import { accordian } from "./types/accordian.mjs";
 import { dropdown } from "./types/dropdown.mjs";
 
-const funcs = ["function1", "function2", "function3", "function4", "function5"];
+const funcs = [
+  "function1",
+  "function2",
+  "function3",
+  "function4",
+  "function5",
+  "function6",
+  "function7",
+  "function8",
+  "function9",
+  "function10",
+];
 
 export function Functions(functions) {
   let functionsHtml = "";
 
   for (const func of functions) {
+    console.log(func, "func");
     const body = `function: ${dropdown(funcs, func.function)}<br>`;
     functionsHtml += `${accordian("function", func.function, body, true)} <br>`;
   }
