f78a49ea2e00d07a2136e1a4923db99a6924233a
Nicholas Alvén <nicholas@nikolaidev.com>
2023-02-13 18:49:04 +0100

    auto commit to dev

--- cms/functions/views.mjs
+++ cms/functions/views.mjs
@@ -6,4 +6,13 @@ export async function views() {
   const views = data.views;
 
   console.log("views: ", views);
+
+  for (const view of views) {
+    const { name, id } = view;
+    const viewDiv = document.createElement("div");
+    viewDiv.id = id;
+    viewDiv.className = "view";
+    viewDiv.innerHTML = `<h2>${name}</h2>`;
+    document.body.appendChild(viewDiv);
+  }
 }
