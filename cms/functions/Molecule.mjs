0f5d0dfd706249b960bf58451232ff8d443c7047
Nicholas Alvén <nicholas@nikolaidev.com>
2023-02-14 00:23:27 +0100

    auto commit to dev

--- cms/functions/Molecule.mjs
+++ cms/functions/Molecule.mjs
@@ -1,26 +1,13 @@
 import { accordian } from "./types/accordian.mjs";
-import { dropdown } from "./types/dropdown.mjs";
 import { Functions } from "./Functions.mjs";
-
-const mols = [
-  "molecule1",
-  "molecule2",
-  "molecule3",
-  "molecule4",
-  "molecule5",
-  "molecule6",
-  "molecule7",
-  "molecule8",
-  "molecule9",
-  "molecule10",
-];
+import { Atoms } from "./Atoms.mjs";
 
 export function Molecules(molecules) {
   let moleculesHtml = "";
 
   for (const mol of molecules) {
     const body = `${Functions(mol.functions)}<br>
-    atoms: ATOMS`;
+    ${Atoms(mol.atoms)}`;
     moleculesHtml += `${accordian("molecule", mol.molecule, body, true)} <br>`;
   }
 
