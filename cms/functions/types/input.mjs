98fa12ae36903fe4e60486dfb5c8bc1204b5f459
Nicholas Alvén <nicholas@nikolaidev.com>
2023-02-13 22:32:17 +0100

    auto commit to dev

--- cms/functions/types/input.mjs
+++ cms/functions/types/input.mjs
@@ -1,12 +1,10 @@
 import { writeModel } from "./requests/writeModel.mjs";
 
 export function input(key, value) {
-  const label = document.createElement("label");
-  label.textContent = key;
+  const input = `
+      <label>${key}:</label>
+      <input value="${value}">
+    `;
 
-  const input = document.createElement("input");
-  input.setAttribute("value", value);
-  input.addEventListener("change", writeModel);
-
-  return { label, input };
+  return input;
 }
