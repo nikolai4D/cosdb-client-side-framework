bceac948cbe3da7ece880ab8bb745afec8db66fe
Nicholas Alvén <nicholas@nikolaidev.com>
2023-02-13 22:32:17 +0100

    auto commit to dev

--- cms/functions/types/accordian.mjs
+++ cms/functions/types/accordian.mjs
@@ -1,7 +1,9 @@
+import { input } from "./input.mjs";
+
 export function accordian(key, value, body) {
   return `   
     <div class="accordion">
-        <div class="accordion-header">${key}: ${value}</div>
+        <div class="accordion-header">${input(key, value)}</div>
         <div class="accordion-body">${body}</div>
     </div>`;
 }
