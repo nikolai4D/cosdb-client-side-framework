d1f2ce7427b942851ad662bf6ae23757f68bc6bb
Nicholas Alvén <nicholas@nikolaidev.com>
2023-02-13 23:11:21 +0100

    auto commit to dev

--- cms/functions/View.mjs
+++ cms/functions/View.mjs
@@ -14,6 +14,6 @@ export function View(view) {
 
   return `   
       <div class="view">
-          ${accordian(key, value, body, "false")}
+          ${accordian(key, value, body)}
       </div>`;
 }
--- cms/functions/types/input.mjs
+++ cms/functions/types/input.mjs
@@ -1,9 +1,9 @@
 // import { writeModel } from "./requests/writeModel.mjs";
 
-export function input(key, value, keyDisabled) {
+export function input(key, value, keyDisabled = "") {
   const input = `
       <label>${key}:</label>
-      <input value="${value}" disabled="${keyDisabled}">
+      <input value="${value}" ${keyDisabled}">
     `;
 
   return input;
