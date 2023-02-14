2dbcfe5f7a4d11e4d3098c7bc680dc95a0064266
Nicholas Alvén <nicholas@nikolaidev.com>
2023-02-14 10:50:39 +0100

    auto commit to dev

--- cms/functions/views.mjs
+++ cms/functions/views.mjs
@@ -17,20 +17,13 @@ export async function views() {
   const createViewButton = document.createElement("button");
   createViewButton.textContent = "Create View";
   createViewButton.classList.add("createViewButton");
-  createViewButton.addEventListener("click", () => {
+  createViewButton.addEventListener("click", async () => {
     console.log("add button clicked");
     const div = document.createElement("div");
     const view = { view: "New View", viewTemplate: "", slots: [] };
     div.innerHTML = View(view);
-    div.innerHTML.addEventListener("click", function (event) {
-      const header = event.target.closest(".accordion-header");
-      if (header) {
-        const accordion = header.parentElement;
-        const accordionBody = accordion.querySelector(".accordion-body");
-        accordionBody.classList.toggle("open");
-      }
-    });
     document.body.insertBefore(div, document.body.children[1]);
+    await accordian();
   });
   document.body.insertBefore(createViewButton, document.body.firstChild);
 
