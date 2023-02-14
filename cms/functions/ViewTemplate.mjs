7717781aa317519139a8e293c08fd447a7181f5f
Nicholas Alvén <nicholas@nikolaidev.com>
2023-02-14 09:10:15 +0100

    auto commit to dev

--- cms/functions/ViewTemplate.mjs
+++ cms/functions/ViewTemplate.mjs
@@ -1,6 +1,7 @@
 import { accordian } from "./types/accordian.mjs";
 import { Functions } from "./Functions.mjs";
 import { Slots } from "./slots.mjs";
+import { dropdown } from "./types/dropdown.mjs";
 
 const viewTemps = ["viewTemplate1", "viewTemplate2", "viewTemplate3"];
 
