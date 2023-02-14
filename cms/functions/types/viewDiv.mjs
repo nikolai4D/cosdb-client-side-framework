7f17f15903aefd50877a19cae7da489a5e791d41
Nicholas Alvén <nicholas@nikolaidev.com>
2023-02-14 20:07:41 +0100

    auto commit to dev

---
+++ cms/functions/types/viewDiv.mjs
@@ -0,0 +1,10 @@
+import { View } from "../View.mjs";
+
+export function viewDiv(view) {
+  const viewDiv = document.createElement("div");
+  viewDiv.classList.add("view");
+  const viewContent = View(view);
+  viewDiv.appendChild(viewContent);
+
+  return viewDiv;
+}
