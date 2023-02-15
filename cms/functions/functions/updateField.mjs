82af2aae551d9e12d8cfca647460d84d0cdd19b1
Nicholas Alvén <nicholas@nikolaidev.com>
2023-02-15 23:22:59 +0100

    auto commit to dev

--- cms/functions/functions/updateField.mjs
+++ cms/functions/functions/updateField.mjs
@@ -4,15 +4,15 @@ export function updateField(json, id, newValue) {
     if (typeof json[prop] === "object") {
       // If this is an object, recursively call the same function on it
       updateField(json[prop], id, newValue);
+    } else if (prop === "atomId" && json[prop] === id) {
+      // If this is an atom object, update the option value
+      if (json.hasOwnProperty("option")) {
+        json["option"] = newValue;
+      }
     } else if (prop.endsWith("Id") && json[prop] === id) {
       // If this is the object we want to update, update the specified value
       const updateKey = prop.replace("Id", "");
-      if (updateKey === "option") {
-        // If this is an atom object, update the option value
-        json[prop] = newValue;
-      } else {
-        json[updateKey] = newValue;
-      }
+      json[updateKey] = newValue;
     } else if (prop.startsWith("function") && typeof json[prop] === "object") {
       // If this is a function object, recursively call the function on it
       updateField(json[prop], id, newValue);
