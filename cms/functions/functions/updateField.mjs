60d19ea609ec25e21bc1f5020cf57194369373f3
Nicholas Alvén <nicholas@nikolaidev.com>
2023-02-15 23:55:21 +0100

    auto commit to dev

--- cms/functions/functions/updateField.mjs
+++ cms/functions/functions/updateField.mjs
@@ -9,13 +9,20 @@ export function updateField(json, id, newValue) {
       if (json.hasOwnProperty("option")) {
         json["option"] = newValue;
       }
+    } else if (
+      prop.startsWith("function") &&
+      !prop.endsWith("Id") &&
+      typeof json[prop] === "string"
+    ) {
+      // If this is a function object, and not a functionId, update the specified value
+      json[prop] = newValue;
     } else if (prop.endsWith("Id") && json[prop] === id) {
       // If this is the object we want to update, update the specified value
       const updateKey = prop.replace("Id", "");
       json[updateKey] = newValue;
     } else if (prop.startsWith("function") && typeof json[prop] === "object") {
-      // If this is a function object, update the specified value
-      json[prop] = newValue;
+      // If this is a function object, recursively call the function on it
+      updateField(json[prop], id, newValue);
     }
   }
   return json;
