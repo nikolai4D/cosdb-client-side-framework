c2e646ae62eee3853b1f670d16b4e696d097e185
Nicholas Alvén <nicholas@nikolaidev.com>
2023-02-16 00:37:07 +0100

    auto commit to dev

--- cms/functions/functions/updateField.mjs
+++ cms/functions/functions/updateField.mjs
@@ -4,34 +4,17 @@ export function updateField(json, id, newValue) {
     if (typeof json[prop] === "object") {
       // If this is an object, recursively call the same function on it
       updateField(json[prop], id, newValue);
-    } else if (prop === "atomId" && json[prop] === id) {
-      // If this is an atom object, update the option value
-      if (json.hasOwnProperty("option")) {
-        json["option"] = newValue;
-      }
-    } else if (
-      prop.startsWith("function") &&
-      prop !== "function" &&
-      !prop.endsWith("Id") &&
-      typeof json[prop] === "string"
-    ) {
-      // If this is a function object, and not a functionId, update the specified value
-      const regex = /^function\d+$/;
-      if (regex.test(prop)) {
-        const updateKey = prop;
-        json[updateKey] = newValue;
-      }
-    } else if (
-      prop.startsWith("function") &&
-      prop !== "function" &&
-      typeof json[prop] === "object"
-    ) {
-      // If this is a function object, recursively call the function on it
-      updateField(json[prop], id, newValue);
     } else if (prop.endsWith("Id") && json[prop] === id) {
       // If this is the object we want to update, update the specified value
       const updateKey = prop.replace("Id", "");
       json[updateKey] = newValue;
+      if (updateKey === "atom" && json.hasOwnProperty("option")) {
+        // If this is an atom object, update the option value
+        json["option"] = newValue;
+      } else if (updateKey === "function" && json.hasOwnProperty("option")) {
+        // If this is a function object, update the option value
+        json["option"] = newValue;
+      }
     }
   }
   return json;
