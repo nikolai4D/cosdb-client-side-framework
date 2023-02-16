1debb83aa7f5e40ab207cb453ce1c2ed91064923
Nicholas Alvén <nicholas@nikolaidev.com>
2023-02-16 07:54:09 +0100

    auto commit to dev

--- cms/functions/types/viewDiv.mjs
+++ cms/functions/types/viewDiv.mjs
@@ -3,6 +3,16 @@ import { View } from "../View.mjs";
 export function ViewDiv(view) {
   const viewDiv = document.createElement("div");
   viewDiv.classList.add("view");
+
+  // Create delete button
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "x";
+  deleteButton.onclick = () => {
+    if (confirm("Are you sure you want to delete this view?")) {
+      viewDiv.remove();
+    }
+  };
+  viewDiv.appendChild(deleteButton);
   const viewContent = View(view);
   viewDiv.appendChild(viewContent);
 
