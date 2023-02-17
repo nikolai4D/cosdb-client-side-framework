cbf57ed2d9d5dca7b6e6d85edd31bc01f0b98487
Nicholas Alvén <nicholas@nikolaidev.com>
2023-02-17 08:09:41 +0100

    auto commit to dev

--- cms/index.mjs
+++ cms/index.mjs
@@ -1,6 +1,3 @@
 import { cms_app } from "./cms-app/cms-app.mjs";
-import { getComponentsHierarchy } from "./cms-app/requests/getComponentsHierarchy.mjs";
-
-console.log(getComponentsHierarchy());
 
 window.onload = cms_app;
