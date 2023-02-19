fffd33910138ada1667bd27661f454c04440a5d6
Nicholas Alvén <nicholas@nikolaidev.com>
2023-02-19 19:09:48 +0100

    auto commit to dev

--- cms/cms-app/create/createViewTemplateDom.mjs
+++ cms/cms-app/create/createViewTemplateDom.mjs
@@ -1,5 +1,7 @@
 export async function createViewTemplateDom() {
   console.log("reached createViewTemplateDom()");
+  const pElement = document.createElement("p");
+  pElement.textContent = "createViewTemplateDom";
 
-  return "<p>createViewTemplateDom</p>";
+  return pElement;
 }
