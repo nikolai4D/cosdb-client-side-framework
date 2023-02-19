9bd9ea3783eb9092e8d8c20a6cba59d347abfaca
Nicholas Alvén <nicholas@nikolaidev.com>
2023-02-19 18:36:22 +0100

    auto commit to dev

--- cms/cms-app/requests/getUuid.mjs
+++ cms/cms-app/requests/getUuid.mjs
@@ -1,9 +1,9 @@
 export async function getUuid() {
   try {
     const response = await fetch("/getuuid");
-    // const data = await response.json();
+    const data = await response.json();
     //console.log(data);
-    return response;
+    return data;
   } catch (error) {
     console.error("An error occurred while fetching the data:", error);
   }
