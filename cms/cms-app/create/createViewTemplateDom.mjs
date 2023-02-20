8d97d227aee830e9b6061d108c01974ac20aa069
Nicholas Alvén <nicholas@nikolaidev.com>
2023-02-20 06:50:41 +0100

    auto commit to dev

--- cms/cms-app/create/createViewTemplateDom.mjs
+++ cms/cms-app/create/createViewTemplateDom.mjs
@@ -1,7 +1,48 @@
-export async function createViewTemplateDom(viewTemplateData) {
-  console.log(viewTemplateData, "viewTemplateData");
-  const pElement = document.createElement("p");
-  pElement.textContent = "createViewTemplateDom";
+// export async function createViewTemplateDom(viewTemplateData) {
+//   console.log(viewTemplateData, "viewTemplateData");
+//   const pElement = document.createElement("p");
+//   pElement.textContent = "createViewTemplateDom";
 
-  return pElement;
+//   return pElement;
+// }
+
+import { accordionDropdown } from "../types/accordionDropdown.mjs";
+import { Slots } from "./Slots.mjs";
+import { readComponents } from "../requests/readComponents.mjs";
+
+const type = "viewTemplates";
+
+export async function createViewTemplateDom(viewTemplateData) {
+  let components = (await readComponents(type)).map(
+    (component) => component.name
+  );
+  console.log(components);
+  components = ["", ...components];
+
+  const viewTemplateDiv = document.createElement("div");
+  viewTemplateDiv.classList.add("viewTemplate");
+
+  const selectedValue = viewTemplateData.option;
+  const values = components;
+  const key = "viewTemplate";
+  const id = viewTemplateData.id;
+
+  const pElement = document.createElement("p");
+  pElement.textContent = "Slots for viewTemplate";
+
+  const bodyDiv = document.createElement("div");
+  bodyDiv.appendChild(pElement);
+
+  const accordionDiv = accordionDropdown(
+    bodyDiv,
+    key,
+    values,
+    selectedValue,
+    id,
+    false
+  );
+
+  viewTemplateDiv.appendChild(accordionDiv);
+
+  return viewTemplateDiv;
 }
