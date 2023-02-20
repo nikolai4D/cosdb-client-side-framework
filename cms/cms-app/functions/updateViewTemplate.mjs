0d997b92228ad4f282ca28a4ebcaa67ca7a7c60b
Nicholas Alvén <nicholas@nikolaidev.com>
2023-02-20 07:21:33 +0100

    auto commit to dev

--- cms/cms-app/functions/updateViewTemplate.mjs
+++ cms/cms-app/functions/updateViewTemplate.mjs
@@ -14,6 +14,6 @@ export async function updateViewTemplate(json, id, newValue) {
 }
 
 async function importModuleFromFile(filename) {
-  const module = await import(`../../components/viewTemplates/${filename}`);
+  const module = await import(`../../../components/viewTemplates/${filename}`);
   return module;
 }
