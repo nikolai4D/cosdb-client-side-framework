c1301ad54d3ebd799c33e2ac59b3bd2e535ba704
Maja Frankiewicz <majafrank@live.se>
2023-02-20 20:11:45 +0100

    auto commit to dev

--- cms/cms-app/_3_update/updateSlotsDom.mjs
+++ cms/cms-app/_3_update/updateSlotsDom.mjs
@@ -1,9 +1,12 @@
 import { accordionInput } from "../types/accordionInput.mjs";
+import { accordionDropdown } from "../types/accordionDropdown.mjs";
+import { resolveDynamicComponents } from "../requests/resolveDynamicComponents.mjs";
+
 // import { Organisms } from "./Organisms.mjs";
 // import { Molecules } from "./Molecules.mjs";
 // import { Atoms } from "./Atoms.mjs";
 
-export async function updateSlotsDom(slots) {
+export async function updateSlotsDom({slots, components}) {
   const slotDiv = document.createElement("div");
   slotDiv.classList.add("slots");
 
@@ -15,9 +18,36 @@ export async function updateSlotsDom(slots) {
     const id = slot.id;
 
     const bodyDiv = document.createElement("div");
+
+
+
+  const existingViewTemplate = existingModel.views.find(
+    (view) => view.viewTemplate.id === viewTemplateId
+  );
+
+
+  //   const pElement = document.createElement("p");
+
+  //   bodyDiv.appendChild(pElement);
+
+  const selectedValue = viewTemplateData.option;
+  const values = components;
+
+
+  const accordionDiv = await accordionDropdown(
+    bodyDiv,
+    key,
+    values,
+    selectedValue,
+    id,
+    false
+  );
+
+
+  
     // bodyDiv.appendChild(await Organisms(slot));
 
-    const accordionDiv = await accordionInput(bodyDiv, key, value, id, true);
+    // const accordionDiv = await accordionInput(bodyDiv, key, value, id, true);
     slotDiv.appendChild(accordionDiv);
   }
   return slotDiv;
