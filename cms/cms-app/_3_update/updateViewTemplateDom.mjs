5bce15279b7a58b9f7148c8349eb81b6070af7f5
Nicholas Alvén <nicholas@nikolaidev.com>
2023-02-20 13:16:07 +0100

    auto commit to dev

---
+++ cms/cms-app/_3_update/updateViewTemplateDom.mjs
@@ -0,0 +1,10 @@
+function updateViewTemplateDom(viewTemplateData) {
+  const accordionBodyId = "accordion-body-" + id;
+  const accordionBody = document.getElementById(accordionBodyId);
+  if (accordionBody) {
+    while (accordionBody.firstChild) {
+      accordionBody.removeChild(accordionBody.firstChild);
+    }
+    console.log("accordion-body-" + id + " deleted");
+  }
+}
