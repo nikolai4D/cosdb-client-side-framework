d2b7d237b033d4334363c8f3a3e9004dc11aa98b
Nicholas Alvén <nicholas@nikolaidev.com>
2023-02-20 13:15:25 +0100

    auto commit to dev

--- cms/cms-app/functions/eventChangeDropdown.mjs
+++ cms/cms-app/functions/eventChangeDropdown.mjs
@@ -1,4 +1,5 @@
 import { updateViewTemplateData } from "../_3_update/updateViewTemplateData.mjs";
+import { updateViewTemplateDom } from "../_3_update/updateViewTemplateDom.mjs";
 
 export async function eventChangeDropdown(id, key) {
   const select = document.getElementById(id);
@@ -12,26 +13,14 @@ export async function eventChangeDropdown(id, key) {
       selectedValue
     );
 
-    console.log(updatedViewTemplateData, "updatedViewTemplateData");
-
     //update viewTemplateDom
 
-    console.log("changed " + key + ": " + id + "with value: " + selectedValue);
+    console.log(updatedViewTemplateData, "updatedViewTemplateData");
+
+    // const updatedViewTemplateDom = await updateViewTemplateDom(
+    //   updatedViewTemplateData
+    // );
   } else {
     console.log("else");
-    // const updatedModel = await updateField(model, id, selectedValue);
-    // await writeModel(updatedModel);
-    // console.log("changed " + key + ": " + id + "with value: " + selectedValue);
   }
 }
-
-// function updateViewTemplateAccordionBody(id) {
-//   const accordionBodyId = "accordion-body-" + id;
-//   const accordionBody = document.getElementById(accordionBodyId);
-//   if (accordionBody) {
-//     while (accordionBody.firstChild) {
-//       accordionBody.removeChild(accordionBody.firstChild);
-//     }
-//     console.log("accordion-body-" + id + " deleted");
-//   }
-// }
