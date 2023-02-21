8b8f027b5b9a942ce88c1550f835c12c9200d5a1
Maja Frankiewicz <majafrank@live.se>
2023-02-21 10:45:48 +0100

    auto commit to dev

--- cms/cms-app/functions/getConstructors.mjs
+++ cms/cms-app/functions/getConstructors.mjs
@@ -4,10 +4,14 @@ import { getUuid } from "../requests/getUuid.mjs";
 export async function getConstructors(filename, constructorType, type) {
   const file = `${filename}.mjs`;
   const module = await importModuleFromFile(file, filename, type);
-  const constructors = module[constructorType];
-  for (const constructor of constructors) {
-    constructor.id = await getUuid();
-    constructor.option = "";
+  console.log(module)
+
+  const constructors = module[constructorType]
+  if (constructors){
+    for (const constructor of constructors) {
+      constructor.id = await getUuid();
+      constructor.option = "";
+    }
   }
 
   return constructors;
