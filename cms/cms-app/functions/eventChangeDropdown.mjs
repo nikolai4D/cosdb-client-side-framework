5f1a91548f0fb4cf71c4293bd02670af89b90223
Maja Frankiewicz <majafrank@live.se>
2023-02-21 21:37:32 +0100

    auto commit to dev

--- cms/cms-app/functions/eventChangeDropdown.mjs
+++ cms/cms-app/functions/eventChangeDropdown.mjs
@@ -2,6 +2,7 @@ import { updateViewTemplateData } from "../_3_update/updateViewTemplateData.mjs"
 import { updateViewTemplateDom } from "../_3_update/updateViewTemplateDom.mjs";
 import { updateComponentData } from "../_3_update/updateComponentData.mjs";
 import { updateComponentDom } from "../_3_update/updateComponentDom.mjs";
+import { updateFunctionData } from "../_3_update/updateFunctionData.mjs";
 
 export async function eventChangeDropdown(id, key) {
   const select = document.getElementById(id);
@@ -20,7 +21,7 @@ export async function eventChangeDropdown(id, key) {
     return await updateViewTemplateDom(updatedViewTemplateData);
   }
   else if(keytype === "function") {
-    console.log("function!")
+    updateFunctionData()
   }
   else if (keytype === "slot" || keytype === "molecule" || keytype === "atom") {
 
