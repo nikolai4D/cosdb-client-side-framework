5eff9410e7ebbdcf4e599af1dd02da4e2fcfbec1
Maja Frankiewicz <majafrank@live.se>
2023-02-21 16:27:13 +0100

    auto commit to dev

---
+++ cms/cms-app/_3_update/updateSubcomponentDom.mjs
@@ -0,0 +1,42 @@
+import { accordionInput } from "../types/accordionInput.mjs";
+import { accordionDropdown } from "../types/accordionDropdown.mjs";
+
+// import { Organisms } from "./Organisms.mjs";
+// import { Molecules } from "./Molecules.mjs";
+// import { Atoms } from "./Atoms.mjs";
+
+export async function updateSubcomponentDom(subComponents, components) {
+  const subComponentDiv = document.createElement("div");
+  subComponentDiv.classList.add("subComponents");
+
+  for await (const subComponent of subComponents) {
+    // Add if statement to check if subComponent is an organism, molecule, or atom
+
+    const key = subComponent.subComponent;
+    // const value = subComponent.subComponent;
+    const id = subComponent.id;
+
+    const bodyDiv = document.createElement("div");
+
+  const selectedValue = null;
+  const values = components;
+
+
+  const accordionDiv = await accordionDropdown(
+    bodyDiv,
+    key,
+    values,
+    selectedValue,
+    id,
+    false
+  );
+
+
+  
+    // bodyDiv.appendChild(await Organisms(subComponent));
+
+    // const accordionDiv = await accordionInput(bodyDiv, key, value, id, true);
+    subComponentDiv.appendChild(accordionDiv);
+  }
+  return subComponentDiv;
+}
