a43c61d405b1ec3c1b6aac8b8686de28f69a4570
Maja Frankiewicz <majafrank@live.se>
2023-02-21 06:46:12 +0100

    auto commit to dev

--- cms/cms-app/functions/getComponents.mjs
+++ cms/cms-app/functions/getComponents.mjs
@@ -1,17 +1,14 @@
 import { readComponents } from "../requests/readComponents.mjs";
 
 export async function getComponents(types) {
-    console.log(types, "types")
     let allComponents = {}
 
     for (const type of types) {
     
         let components = await readComponents(type)
-
-        console.log(components)
     
             for (const component of components) {
-                allComponents[type] = component;
+                allComponents[type] = component.name;
         }
     }
 
