f1bd604144c3471bb7a283503fc9984facec76d7
Maja Frankiewicz <majafrank@live.se>
2023-02-21 10:59:01 +0100

    auto commit to dev

--- components/organisms/Organism_dummy2.mjs
+++ components/organisms/Organism_dummy2.mjs
@@ -1,11 +1,12 @@
-import { Molecule_dummy1 } from "../atoms/Molecule_dummy1.mjs";
-import { Molecule_dummy2 } from "../atoms/Molecule_dummy2.mjs";
-
 export function Organism_dummy2() {
-  this.molecules = {
-    Molecule_dummy1: new Molecule_dummy1(),
-    Molecule_dummy2: new Molecule_dummy2(),
-  };
+  this.subComponents = [
+    {
+      subComponent: "Molecule_dummy1"
+    },
+    {
+      subComponent: "Molecule_dummy2"
+    }
+  ]
 
   this.functions = {
     function1: null,
@@ -14,8 +15,8 @@ export function Organism_dummy2() {
 
   return `
   <div>
-        <div>${this.molecules.Molecule_dummy1}</div>
-        <div>${this.molecules.Molecule_dummy2}</div>
+    <div>${this.subComponents[0].subComponent}</div>
+    <div>${this.subComponents[1].subComponent}</div>
   </div>
 `;
 }
