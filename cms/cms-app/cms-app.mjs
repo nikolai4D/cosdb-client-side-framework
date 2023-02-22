b7a19b6f35646ecbbb406d564964633b0d255ea1
Nicholas Alvén <nicholas@nikolaidev.com>
2023-02-22 06:08:41 +0100

    auto commit to dev2

--- cms/cms-app/cms-app.mjs
+++ cms/cms-app/cms-app.mjs
@@ -1,10 +1,10 @@
-import { create } from "./functions/create.mjs";
+import { createViewButton } from "./functions/createViewButton.mjs";
 // import { read } from "./_2_read/read.mjs";
 // import { update } from "./_3_update/update.mjs";
 // import { delete } from "./_4_delete/delete.mjs";
 
 export async function cms_app() {
-  await create();
+  await createViewButton();
   // await read();
   // await update();
   // await delete();
