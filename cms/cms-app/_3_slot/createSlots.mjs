07c219dad8dfe7fd3e7353f1da4e8fbf6a4fb4b0
Nicholas Alvén <nicholas@nikolaidev.com>
2023-02-22 07:58:42 +0100

    auto commit to dev2

---
+++ cms/cms-app/_3_slot/createSlots.mjs
@@ -0,0 +1,29 @@
+import { createSlot } from "./createSlot.mjs";
+import { Slot } from "./Slot.mjs";
+import { getConstructors } from "../functions/getConstructors.mjs";
+
+export async function createSlots(viewTemplateBody, id, selectedValue) {
+  const filename = selectedValue;
+  const constructorType = "slots";
+  const type = "viewTemplates";
+  const viewTemplateSlots = await getConstructors(
+    filename,
+    constructorType,
+    type
+  );
+
+  viewTemplateSlots.forEach(async (slot) => {
+    const [[key, value]] = Object.entries(slot);
+
+    const slotKey = key;
+    const slotValues = [];
+    const slotValue = value;
+    const slotParentId = id;
+
+    const newSlot = await Slot(
+      await newSlot(slotKey, slotValues, slotValue, slotParentId)
+    );
+
+    viewTemplateBody.appendChild(newSlot);
+  });
+}
