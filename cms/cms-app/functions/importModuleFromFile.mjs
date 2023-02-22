5d957d6a90f55b83624286680b74570607ee88ae
Nicholas Alvén <nicholas@nikolaidev.com>
2023-02-22 06:07:40 +0100

    auto commit to dev2

--- cms/cms-app/functions/importModuleFromFile.mjs
+++
@@ -1,5 +0,0 @@
-export async function importModuleFromFile(file, filename, type="viewTemplates") {
-  console.log( `../../../components/${type}/${file}`, "path")
-  const module = await import(`../../../components/${type}/${file}`);
-  return new module[filename]();
-}
