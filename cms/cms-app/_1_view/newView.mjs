89e8cfa01de74197ce7abf91cf59d2b3714a6d50
Nicholas Alvén <nicholas@nikolaidev.com>
2023-02-22 06:29:48 +0100

    auto commit to dev2

--- cms/cms-app/_1_view/newView.mjs
+++ cms/cms-app/_1_view/newView.mjs
@@ -5,7 +5,7 @@ const view = {}
 
 view.customType = "view"
 view.key = "view"
-view.value "New View"
+view.value = "New View"
 view.id = await getUuid();
 view.parentId; = view.id
 view.valueDisabled = false;
