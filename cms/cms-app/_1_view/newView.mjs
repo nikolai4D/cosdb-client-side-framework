88bd07b45536e882f77f6f5ccec10b05e53ba785
Nicholas Alvén <nicholas@nikolaidev.com>
2023-02-22 06:29:15 +0100

    auto commit to dev2

---
+++ cms/cms-app/_1_view/newView.mjs
@@ -0,0 +1,14 @@
+import { getUuid } from "../requests/getUuid.mjs";
+
+export function newView(){
+const view = {}
+
+view.customType = "view"
+view.key = "view"
+view.value "New View"
+view.id = await getUuid();
+view.parentId; = view.id
+view.valueDisabled = false;
+
+return view
+}
--- cms/cms-app/types/accordion.mjs
+++ cms/cms-app/types/accordion.mjs
@@ -1,7 +1,15 @@
 import { readModel } from "../requests/readModel.mjs";
 import { writeModel } from "../requests/writeModel.mjs";
 
-export async function accordion(header, body, id, key) {
+export async function accordion(
+  headerContent,
+  bodyContent,
+  customType,
+  key,
+  value,
+  id,
+  parentId
+) {
   const accordion = document.createElement("div");
   accordion.classList.add("accordion");
 
@@ -9,11 +17,7 @@ export async function accordion(header, body, id, key) {
 
   const headerAccordion = document.createElement("div");
   headerAccordion.classList.add("accordion-header");
-  headerAccordion.appendChild(header);
-
-  if (key === "view") {
-    deleteButton(id, accordion, headerAccordion);
-  }
+  headerAccordion.appendChild(headerContent);
 
   headerAccordion.addEventListener("click", (event) => {
     if (event.target.tagName === "OPTION") {
@@ -26,35 +30,11 @@ export async function accordion(header, body, id, key) {
   const bodyEl = document.createElement("div");
   bodyEl.classList.add("accordion-body");
   bodyEl.id = bodyId;
-  console.log(await body);
 
-  bodyEl.appendChild(await body);
+  bodyEl.appendChild(await bodyContent);
 
   accordion.appendChild(headerAccordion);
   accordion.appendChild(bodyEl);
 
   return accordion;
-}
-
-async function deleteView(id) {
-  const json = await readModel();
-  const filteredViews = json.views.filter((view) => view.viewId !== id);
-  json.views = filteredViews;
-
-  await writeModel(json);
-}
-
-function deleteButton(id, accordion, headerAccordion) {
-  const deleteButton = document.createElement("button");
-  deleteButton.classList.add("delete-view-button");
-  deleteButton.textContent = "x";
-  deleteButton.id = id;
-  deleteButton.addEventListener("click", () => {
-    if (confirm("Are you sure you want to delete this view?")) {
-      deleteView(id);
-      accordion.remove();
-      console.log("delete " + id);
-    }
-  });
-  headerAccordion.appendChild(deleteButton);
 }
