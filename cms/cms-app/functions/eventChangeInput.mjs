5135fe206f0bfc81eeeb018af4166c91f637fed4
Nicholas Alvén <nicholas@nikolaidev.com>
2023-02-22 07:09:22 +0100

    auto commit to dev2

--- cms/cms-app/functions/eventChangeInput.mjs
+++ cms/cms-app/functions/eventChangeInput.mjs
@@ -1,10 +1,12 @@
-import { readModel } from "../requests/readModel.mjs";
-import { updateField } from "../functions/updateField.mjs";
-import { writeModel } from "../requests/writeModel.mjs";
+// import { readModel } from "../requests/readModel.mjs";
+// import { updateField } from "../functions/updateField.mjs";
+// import { writeModel } from "../requests/writeModel.mjs";
 
-export async function eventChangeInput(id, value) {
-  console.log("changed id: " + id + "with value: " + value);
-  const modelJson = await readModel();
-  const updatedModelJson = await updateField(modelJson, id, value);
-  await writeModel(updatedModelJson);
+export async function eventChangeInput(id) {
+  const input = document.getElementById(id);
+  const value = input.value;
+  const customType = select.getAttribute("customType");
+  const parentId = select.getAttribute("parentId");
+
+  console.log("update: ", customType, ": ", { id, parentId, value });
 }
