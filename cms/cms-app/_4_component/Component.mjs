ee3b07092ef0ecacbe6157c98e008e90268ad77b
Nicholas Alvén <nicholas@nikolaidev.com>
2023-02-22 09:20:17 +0100

    auto commit to dev2

---
+++ cms/cms-app/_4_component/Component.mjs
@@ -0,0 +1,33 @@
+import { accordionDropdown } from "../types/accordionDropdown.mjs";
+
+export async function Component(component) {
+  const ComponentDiv = document.createElement("div");
+  ComponentDiv.classList.add(Component.customType);
+
+  const customType = Component.customType;
+  const key = Component.key;
+  const values = Component.values;
+  const selectedValue = Component.selectedValue;
+  const id = Component.id;
+  const parentId = Component.parentId;
+  const valueDisabled = Component.valueDisabled;
+
+  const bodyDiv = document.createElement("div");
+  const contentDiv = document.createElement("div"); //await ?;
+  bodyDiv.appendChild(contentDiv);
+
+  const ComponentAccordionDropdown = await accordionDropdown(
+    bodyDiv,
+    customType,
+    key,
+    values,
+    selectedValue,
+    id,
+    parentId,
+    valueDisabled
+  );
+
+  ComponentDiv.appendChild(ComponentAccordionDropdown);
+
+  return ComponentDiv;
+}
