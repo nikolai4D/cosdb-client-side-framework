e1fd5bccab0461a7e060b5fafb666af616dac66b
Maja Frankiewicz <majafrank@live.se>
2023-02-23 12:06:41 +0100

    auto commit to dev2

--- cms/cms-app/_7_atom/createAtom.mjs
+++ cms/cms-app/_7_atom/createAtom.mjs
@@ -1,43 +1,18 @@
-
-import { Molecule } from "./Molecule.mjs";
-import { newMolecule } from "./newMolecule.mjs";
 import { Atom } from "../_7_atom/Atom.mjs";
 import { newAtom } from "../_7_atom/newAtom.mjs";
 import { Function } from "../_8_function/Function.mjs";
 import { newFunction } from "../_8_function/newFunction.mjs";
 import { getConstructors } from "../functions/getConstructors.mjs";
 
-export async function createMolecule(componentBody, id, selectedValue) {
+export async function createAtom(componentBody, id, selectedValue) {
   const filename = selectedValue;
-  const type = "molecules";
+  const type = "atoms";
   const organismBody = document.createElement("div");
 
 
   //--------------------------------
 
-  //get Molecules
-
-  const constructorTypeMolecules = "molecules";
-
-  const componentMolecules = await getConstructors(
-    filename,
-    constructorTypeMolecules,
-    type
-    );
-
-  if (componentMolecules) {
-    await createSubMoleculesEl(
-      componentMolecules,
-      id,
-      organismBody,
-      componentBody
-    );
-  }
-
-
-  //--------------------------------
-
-  //get Molecules
+    //get Atoms
 
   const constructorTypeAtoms = "atoms";
 
@@ -79,24 +54,6 @@ export async function createMolecule(componentBody, id, selectedValue) {
   }
 }
 
-function createSubMoleculesEl(subComps, id, compBody, parentBody) {
-  subComps.forEach(async (comp) => {
-    const [[key, value]] = Object.entries(comp);
-    console.log({ key, value });
-
-    const organismKey = key;
-    const organismValue = value;
-    const organismParentId = id;
-
-    let childSlot = await Molecule(
-      await newMolecule(organismKey, organismValue, organismParentId),
-      compBody
-    )
-
-    parentBody.appendChild(childSlot);
-
-  });
-}
 
 function createSubAtomsEl(subComps, id, compBody, parentBody) {
     subComps.forEach(async (comp) => {
