f83aaf7e0f9f25907cf5eda61bbdf6b3b1b5d434
Maja Frankiewicz <majafrank@live.se>
2023-02-23 12:16:20 +0100

    auto commit to dev2

--- cms/cms-app/_6_molecule/createMolecule.mjs
+++ cms/cms-app/_6_molecule/createMolecule.mjs
@@ -34,6 +34,7 @@ export async function createMolecule(componentBody, id, selectedValue) {
     );
   }
 
+  console.log({selectedValue, filename, type, componentMolecules})
 
   //--------------------------------
 
