5edb920330a6d8d17637eb571f9f55bfdb8a3f87
Maja Frankiewicz <majafrank@live.se>
2023-02-23 13:33:24 +0100

    auto commit to dev2

---
+++ components/atoms/Atom_dummy3.mjs
@@ -0,0 +1,12 @@
+export function Atom_dummy3() {
+  this.valueOptions = [
+    {
+      valueOption: "valueOption3",
+    }
+  ]
+  return (
+  `  <div>
+      <h1>${this.valueOption}</h1>
+    </div>
+  `);
+}
