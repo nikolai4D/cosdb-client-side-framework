572ac4f138fc5edc8e9d93dac3d68b2973b63329
Maja Frankiewicz <majafrank@live.se>
2023-02-23 16:01:20 +0100

    auto commit to dev2

--- cms/cms-app/requests/updateModel.mjs
+++ cms/cms-app/requests/updateModel.mjs
@@ -5,7 +5,7 @@ import { writeModel } from "./writeModel.mjs";
 export async function updateModel(id, value, parentId="", type) {
     let existingModel = await readModel();
   
-    console.log(existingModel);
+    console.log(existingModel, type);
   
       existingModel[type].push({
         id,
