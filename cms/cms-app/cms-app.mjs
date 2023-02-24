65505c7556a2921865953812a06d61964770f085
Maja Frankiewicz <majafrank@live.se>
2023-02-24 13:18:34 +0100

    auto commit to dev2

--- cms/cms-app/cms-app.mjs
+++ cms/cms-app/cms-app.mjs
@@ -1,10 +1,12 @@
 import { createViewButton } from "./functions/createViewButton.mjs";
+import { State } from "./State.mjs"
 // import { read } from "./_2_read/read.mjs";
 // import { update } from "./_3_update/update.mjs";
 // import { delete } from "./_4_delete/delete.mjs";
 
 export async function cms_app() {
   await createViewButton();
+  console.log({State})
   // await read();
   // await update();
   // await delete();
