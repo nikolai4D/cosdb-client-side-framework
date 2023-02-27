3df5b1edfa0c3acf7b0e20a6135c03e614ca06e8
Maja Frankiewicz <majafrank@live.se>
2023-02-27 15:52:04 +0100

    auto commit to dev2

--- cms/cms-app/cms-app.mjs
+++ cms/cms-app/cms-app.mjs
@@ -1,10 +1,13 @@
 import { createViewButton } from "./functions/createViewButton.mjs";
+import { createSaveButton } from "./functions/createSaveButton.mjs";
 // import { read } from "./_2_read/read.mjs";
 // import { update } from "./_3_update/update.mjs";
 // import { delete } from "./_4_delete/delete.mjs";
 
 export async function cms_app() {
   await createViewButton();
+  await createSaveButton();
+
   // await read();
   // await update();
   // await delete();
