ef78e0bd60f76402cf24da07e99e22ee207f00b8
Maja Frankiewicz <majafrank@live.se>
2023-02-27 15:06:27 +0100

    auto commit to dev2

--- cms/cms-app/functions/eventChangeDropdown.mjs
+++ cms/cms-app/functions/eventChangeDropdown.mjs
@@ -26,7 +26,8 @@ export async function eventChangeDropdown(id) {
 }
   const getDomAndData = async (customType, id, selectedValue, parentId) => {
      await getByCustomType(customType, id, selectedValue, parentId);
-     setTimeout( await action.updateModel(State), 10000); // or whatever delay you want to use
+     await action.updateModel(State)
+    //  setTimeout( await action.updateModel(State), 10000); // or whatever delay you want to use
 
     
   }
