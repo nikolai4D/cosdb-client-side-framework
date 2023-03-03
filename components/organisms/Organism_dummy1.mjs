78d84324d8f916662278c7ae986dba10ff0c6854
Maja Frankiewicz <majafrank@live.se>
2023-03-03 11:17:04 +0100

    auto commit to dev

--- components/organisms/Organism_dummy1.mjs
+++ components/organisms/Organism_dummy1.mjs
@@ -1,10 +1,12 @@
 export function Organism_dummy1() {
-  this.organisms = [
-    {
-      organism: "Organism_dummy2"
+  Component.call(this);
+
+  this.subComponents = [
+    { type: "organism",
+      title: "Organism_dummy2"
     },
-    {
-      organism: "Organism_dummy3"
+    { type: "organism",
+      title: "Organism_dummy3"
     }
   ]
 
@@ -17,10 +19,43 @@ export function Organism_dummy1() {
     }
   ]
 
-  return `
-  <div>
-    <div>${this.organisms[0].organism}</div>
-    <div>${this.organisms[1].organism}</div>
+
+  this.getHtml = function(){
+    return `<div>
+    <div>${this.subComponents[0].title}</div>
+    <div>${this.subComponents[1].title}</div>
   </div>
-`;
+  `;
+  }
 }
+
+// export function Organism_dummy1() {
+//   Component.call(this);
+
+//   this.organisms = [
+//     {
+//       organism: "Organism_dummy2"
+//     },
+//     {
+//       organism: "Organism_dummy3"
+//     }
+//   ]
+
+//   this.functions = [
+//     {
+//       function: "function3",
+//     },
+//     {
+//       function: "function4",
+//     }
+//   ]
+
+
+//   this.getHtml = function(){
+//     return `<div>
+//     <div>${this.organisms[0].organism}</div>
+//     <div>${this.organisms[1].organism}</div>
+//   </div>
+//   `;
+//   }
+// }
