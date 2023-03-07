232974e3372a323c6bc928bce9289bfde504739b
Nicholas Alvén <nicholas@nikolaidev.com>
2023-03-07 08:05:46 +0100

    auto commit to dev

--- components/organisms/Organism_dummy1.mjs
+++ components/organisms/Organism_dummy1.mjs
@@ -1,43 +1,12 @@
-export function Organism_dummy1() {
-  Component.call(this);
-
-  this.subComponents = [
-    { type: "organism",
-      title: "Organism_dummy2"
-    },
-    { type: "organism",
-      title: "Organism_dummy3"
-    }
-  ]
-
-  this.functions = [
-    {
-      function: "function3",
-    },
-    {
-      function: "function4",
-    }
-  ]
-
-
-  this.getHtml = function(){
-    return `<div>
-    <div>${this.subComponents[0].title}</div>
-    <div>${this.subComponents[1].title}</div>
-  </div>
-  `;
-  }
-}
-
 // export function Organism_dummy1() {
 //   Component.call(this);
 
-//   this.organisms = [
-//     {
-//       organism: "Organism_dummy2"
+//   this.subComponents = [
+//     { type: "organism",
+//       title: "Organism_dummy2"
 //     },
-//     {
-//       organism: "Organism_dummy3"
+//     { type: "organism",
+//       title: "Organism_dummy3"
 //     }
 //   ]
 
@@ -50,12 +19,41 @@ export function Organism_dummy1() {
 //     }
 //   ]
 
-
 //   this.getHtml = function(){
 //     return `<div>
-//     <div>${this.organisms[0].organism}</div>
-//     <div>${this.organisms[1].organism}</div>
+//     <div>${this.subComponents[0].title}</div>
+//     <div>${this.subComponents[1].title}</div>
 //   </div>
 //   `;
 //   }
 // }
+
+export function Organism_dummy1() {
+  Component.call(this);
+
+  this.organisms = [
+    {
+      organism: "Organism_dummy2",
+    },
+    {
+      organism: "Organism_dummy3",
+    },
+  ];
+
+  this.functions = [
+    {
+      function: "function3",
+    },
+    {
+      function: "function4",
+    },
+  ];
+
+  this.getHtml = function () {
+    return `<div>
+    <div>${this.organisms[0].organism}</div>
+    <div>${this.organisms[1].organism}</div>
+  </div>
+  `;
+  };
+}
