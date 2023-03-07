13f75265302c1f44829bfa3a7ca72653b6dc5f0b
Nicholas Alvén <nicholas@nikolaidev.com>
2023-03-07 07:33:22 +0100

    auto commit to dev

--- cms/cms-app2/functions/getComponents.mjs
+++
@@ -1,9 +0,0 @@
-import { readComponents } from "../requests/readComponents.mjs";
-
-export async function getComponents(type) {
-    let components = (await readComponents(type)).map(
-        (component) => component.name
-    );
-
-    return components;
-}
