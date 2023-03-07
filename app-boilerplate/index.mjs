13498eeb219e5ade8b4403ab3e0346c76aeb081f
Maja Frankiewicz <majafrank@live.se>
2023-03-07 20:38:16 +0100

    auto commit to CSFO-1-app-connect-router-to-app-and-show-components-from-model-json

--- app-boilerplate/index.mjs
+++ app-boilerplate/index.mjs
@@ -2,13 +2,14 @@ import {Router, route} from "../core/Router.mjs";
 import {Controller} from "./helpers/Controller.mjs";
 import { readModel } from "./helpers/readModel.mjs";
 
-export const router = new Router([
-    route("", Controller)
-])
-
-readModel();
 const path = window.location.pathname.slice(1)
 console.log("path: " , path)
 
-router.goTo(path)
+readModel();
 
+export const router = new Router([
+    route(path, Controller)
+])
+
+
+router.goTo(path)
