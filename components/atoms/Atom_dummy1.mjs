987c1c9cfa428c2c681cd1a1aea10d43a1a05163
Nicholas Alvén <nicholas@nikolaidev.com>
2023-03-07 22:11:28 +0100

    auto commit to b

--- components/atoms/Atom_dummy1.mjs
+++ components/atoms/Atom_dummy1.mjs
@@ -1,8 +1,8 @@
 export function Atom_dummy1() {
-  this.value = "value1";
+  this.value = { value: "value1" };
 
   return `<div>
-      <h1>${this.value}</h1>
+      <h1>${this.value.value}</h1>
     </div>
   `;
 }
--- components/atoms/Atom_dummy3.mjs
+++ components/atoms/Atom_dummy3.mjs
@@ -1,8 +1,8 @@
 export function Atom_dummy3() {
-  this.value = "value3";
+  this.value = { value: "value3" };
 
   return `<div>
-          <h1>${this.value}</h1>
-        </div>
-      `;
+        <h1>${this.value.value}</h1>
+      </div>
+    `;
 }
