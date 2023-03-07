b5695e416e300711c25742886bacb01f10310e58
Nicholas Alvén <nicholas@nikolaidev.com>
2023-03-07 22:12:53 +0100

    auto commit to b

--- cms/cms-app/functions/getConstructors.mjs
+++ cms/cms-app/functions/getConstructors.mjs
@@ -7,6 +7,7 @@ export async function getConstructors(filename, constructorType, type) {
   const constructors = module[constructorType];
 
   if (constructors) {
+    console.log("constructors", constructors);
     for (const constructor of constructors) {
       constructor.id = await getUuid();
       constructor.option = "";
