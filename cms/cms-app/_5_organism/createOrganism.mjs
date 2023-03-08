4f732558a98d64c939fb8d712e09a7ab5b1152af
Nicholas Alvén <nicholas@nikolaidev.com>
2023-03-08 06:01:31 +0100

    auto commit to b

--- cms/cms-app/_5_organism/createOrganism.mjs
+++ cms/cms-app/_5_organism/createOrganism.mjs
@@ -92,7 +92,7 @@ function createSubOrganismsEl(subComps, id, compBody, parentBody) {
     let newId = slotEls[0].id;
     let nextLevelBody = document.getElementById("accordion-body-" + newId);
 
-    await createOrganism(nextLevelBody, id, value);
+    await createOrganism(nextLevelBody, newId, value);
   });
 }
 
