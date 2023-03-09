c835357b4675ae48f9bb1475f02c49de112bfa77
Maja Frankiewicz <majafrank@live.se>
2023-03-09 17:48:25 +0100

    auto commit to CSFO-1-app-connect-router-to-app-and-show-components-from-model-json

--- core/Component.mjs
+++ core/Component.mjs
@@ -48,7 +48,7 @@ export function Component(options = {}){
             this.element = stringToHTMLElement(this.getHtml())
             this.deSetComponentsFromString()
             this.bindSlots()
-            await this.bindScript()
+            this.bindScript()
             this.applyStyle()
 
             if(this.id) this.element.id = this.id
