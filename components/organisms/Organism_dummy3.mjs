558f26d8148c9d602e20ae63a6df5c131b5b18cc
Maja Frankiewicz <majafrank@live.se>
2023-03-09 13:55:50 +0100

    auto commit to CSFO-1-app-connect-router-to-app-and-show-components-from-model-json

--- components/organisms/Organism_dummy3.mjs
+++ components/organisms/Organism_dummy3.mjs
@@ -1,10 +1,18 @@
+import { Component } from "../../core/Component.mjs";
+import { slot } from  "../../core/helpers.mjs";
+import { Organism_dummy4 } from "./Organism_dummy4.mjs";
+
 export function Organism_dummy3() {
+  Component.call(this);
+
   this.organisms = [
     {
-      organism: "Organism_dummy4"
+      organism: "Organism_dummy4",
+      component: new Organism_dummy4()
     },
     {
-      organism: "Organism_dummy4"
+      organism: "Organism_dummy4",
+      component: new Organism_dummy4()
     }
   ]
 
@@ -27,10 +35,12 @@ export function Organism_dummy3() {
   ];
 
 
+  this.getHtml = function(){
   return `
   <div>
     <div>${this.organisms[0].organism}</div>
     <div>${this.organisms[1].organism}</div>
   </div>
 `;
+}
 }
