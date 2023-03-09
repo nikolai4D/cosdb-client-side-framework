2206f7f721437db8cea5bf5c74f25b0d2bb1d787
Nicholas Alvén <nicholas@nikolaidev.com>
2023-03-09 17:10:35 +0100

    auto commit to b

--- cms/cms-app/_7_atom/createAtom.mjs
+++ cms/cms-app/_7_atom/createAtom.mjs
@@ -1,8 +1,24 @@
+import { Atom } from "../_7_atom/Atom.mjs";
+import { newAtom } from "../_7_atom/newAtom.mjs";
+
 import { getConstructors } from "../functions/getConstructors.mjs";
 import { input } from "../types/input.mjs";
 import { getUuid } from "../requests/getUuid.mjs";
 
 export async function createAtom(componentBody, id, selectedValue) {
+  const subComponentBody = document.createElement("div");
+  const newAtm = await newAtom("atom", selectedValue, id);
+  const atomSlot = await Atom(newAtm, subComponentBody);
+
+  console.log(newAtm, "newAtm!!!!!!!!!!!!!!");
+  componentBody.appendChild(atomSlot);
+
+  await createSubAtom(subComponentBody, newAtom.id, selectedValue);
+}
+
+export async function createSubAtom(subComponentBody, id, selectedValue) {
+  ///--------------------------------
+
   const filename = selectedValue;
   const type = "atoms";
   const atomBody = document.createElement("div");
@@ -22,7 +38,7 @@ export async function createAtom(componentBody, id, selectedValue) {
   );
 
   if (atomValues) {
-    await createValueEls(atomValues, parentId, atomBody, componentBody);
+    await createValueEls(atomValues, parentId, atomBody, subComponentBody);
   }
 
   function createValueEls(subComps, prntId, compBody, parentBody) {
