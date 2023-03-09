9184d442103f8afa43d2e98e926314d3e13f2b09
Maja Frankiewicz <majafrank@live.se>
2023-03-09 10:15:57 +0100

    auto commit to CSFO-1-app-connect-router-to-app-and-show-components-from-model-json

--- core/View.mjs
+++ core/View.mjs
@@ -13,9 +13,11 @@ export function View() {
     this.setView= async function() {
 
         console.log(await this.template(), "this.template")
+        console.log(await this.template().getElement(), "this.template.getEelkent")
+
 
         if(!this.template) throw new Error("View template is not set")
-        if (!this.template.getElement) document.body.append(this.template().getElement())
+        if (!this.template.getElement) document.body.append(await this.template().getElement())
 
         else document.body.append(this.template.getElement())
 
