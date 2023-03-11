65598d9b487a28ccc1ee00905eecb12acc6ee0ac
Nicholas Alvén <nicholas@nikolaidev.com>
2023-03-11 10:27:33 +0100

    auto commit to b

--- cms/cms-app/data-mgmt/actions/action_writeModel.mjs
+++ cms/cms-app/data-mgmt/actions/action_writeModel.mjs
@@ -2,8 +2,8 @@
 import { writeModel } from "../../requests/writeModel.mjs";
 
 export async function action_writeModel(state) {
-  console.log("writtenModel State: ", state);
-  const writtenModel = await writeModel(state);
+  console.log("writeModel State: ", state);
+  const writtenModel = await writeModel(await state);
   console.log("writtenModel: ", writtenModel);
   return writtenModel;
 }
