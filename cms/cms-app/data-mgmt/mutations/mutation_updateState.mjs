192940d1d4849e61f46ef2a2922246208f904c16
Nicholas Alvén <nicholas@nikolaidev.com>
2023-03-11 13:24:50 +0100

    auto commit to b

--- cms/cms-app/data-mgmt/mutations/mutation_updateState.mjs
+++ cms/cms-app/data-mgmt/mutations/mutation_updateState.mjs
@@ -7,6 +7,14 @@ export async function mutation_updateState(customType, data) {
   if (index !== -1) {
     customTypeData.splice(index, 1, data);
   } else {
+    if (
+      customType === "views" ||
+      customType === "viewTemplates" ||
+      customType === "components"
+    ) {
+      await deleteChildren(id);
+    }
+
     customTypeData.push(data);
   }
 
@@ -14,4 +22,22 @@ export async function mutation_updateState(customType, data) {
 
   //console.log("State, mutation from customType: ", customType, { State });
   return State;
+}
+
+async function deleteChildren(id) {
+  // Find all items in State with parentId equal to id
+  for (const key of Object.keys(State)) {
+    const items = State[key];
+    for (const item of items) {
+      if (item.parentId === id) {
+        await deleteChildren(item.id);
+      }
+    }
+  }
+
+  // Remove all items with parentId equal to id or any of its descendants' id
+  for (const key of Object.keys(State)) {
+    const items = State[key];
+    State[key] = items.filter((item) => item.parentId !== id && item.id !== id);
+  }
 }
