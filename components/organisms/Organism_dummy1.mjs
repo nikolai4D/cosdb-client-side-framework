439ab780e171db65ff6765d1dcdf6a9d2d7e8c53
Nicholas Alvén <nicholas@nikolaidev.com>
2023-03-11 12:55:16 +0100

    auto commit to b

--- components/organisms/Organism_dummy1.mjs
+++ components/organisms/Organism_dummy1.mjs
@@ -1,25 +1,33 @@
-export function Organism_dummy1() {
+export function Organism_dummy3() {
   this.organisms = [
     {
       organism: "Organism_dummy2",
     },
+  ];
+
+  this.molecules = [
     {
-      organism: "Organism_dummy3",
+      molecule: "Molecule_dummy1",
+    },
+    {
+      molecule: "Molecule_dummy2",
     },
   ];
 
   this.functions = [
     {
-      function: "function3",
+      function: "function1",
     },
     {
-      function: "function4",
+      function: "function2",
     },
   ];
 
-  return `<div>
+  return `
+  <div>
     <div>${this.organisms[0].organism}</div>
-    <div>${this.organisms[1].organism}</div>
+    <div>${this.molecules[0].molecule}</div>
+    <div>${this.molecules[1].molecule}</div>
   </div>
-  `;
+`;
 }
