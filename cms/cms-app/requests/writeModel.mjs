ce328e2d13b1c35c5fbae8f66f1109bc8a0873fc
Nicholas Alvén <nicholas@nikolaidev.com>
2023-03-11 10:25:31 +0100

    auto commit to b

--- cms/cms-app/requests/writeModel.mjs
+++ cms/cms-app/requests/writeModel.mjs
@@ -1,12 +1,12 @@
-export async function writeModel(json) {
-  console.log("writeModel: ", json);
+export async function writeModel(state) {
+  console.log("writeModel: ", state);
   try {
     const response = await fetch("/update", {
       method: "PUT",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(json),
+      body: await JSON.stringify(state),
     });
     const result = await response.json();
     return await result;
