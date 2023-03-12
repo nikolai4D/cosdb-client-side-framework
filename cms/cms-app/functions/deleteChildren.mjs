8c222588d40050f791938a432679f2f3b8136ab8
Nicholas Alvén <nicholas@nikolaidev.com>
2023-03-12 15:08:31 +0100

    auto commit to b

--- cms/cms-app/functions/deleteChildren.mjs
+++ cms/cms-app/functions/deleteChildren.mjs
@@ -1,35 +1,40 @@
 import { State } from "../data-mgmt/State.mjs";
 
 export async function deleteChildren(id) {
-  // Find all items in State with parentId equal to id
-  const children = [];
-  for (const key of Object.keys(State)) {
-    if (Array.isArray(State[key])) {
-      const items = State[key];
-      for (const item of items) {
-        if (item.parentId === id) {
-          children.push(item);
+  const stack = [];
+  stack.push(id);
+
+  while (stack.length > 0) {
+    const currentId = stack.pop();
+
+    // Find all items in State with parentId equal to currentId
+    const children = [];
+    for (const key of Object.keys(State)) {
+      if (Array.isArray(State[key])) {
+        const items = State[key];
+        for (const item of items) {
+          if (item.parentId === currentId) {
+            children.push(item);
+          }
         }
       }
     }
-  }
 
-  // Recursively delete all children of the children
-  for (const child of children) {
-    await deleteChildren(child.id);
-  }
+    // Remove all items with parentId equal to currentId or any of its descendants' id
+    for (const key of Object.keys(State)) {
+      if (Array.isArray(State[key])) {
+        const items = State[key];
+        State[key] = items.filter(
+          (item) =>
+            item.parentId !== currentId &&
+            !children.find((child) => child.id === item.parentId)
+        );
+      }
+    }
 
-  console.log("children: ", children);
-
-  // Remove all items with parentId equal to id or any of its descendants' id
-  for (const key of Object.keys(State)) {
-    if (Array.isArray(State[key])) {
-      const items = State[key];
-      State[key] = items.filter(
-        (item) =>
-          item.parentId !== id &&
-          !children.find((child) => child.id === item.parentId)
-      );
+    // Add all children to the stack for processing
+    for (const child of children) {
+      stack.push(child.id);
     }
   }
 }
