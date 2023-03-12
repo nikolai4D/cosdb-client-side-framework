0bc5f5d326033a682e97a5440f0b066b8911cf44
Nicholas Alvén <nicholas@nikolaidev.com>
2023-03-12 17:45:59 +0100

    auto commit to b

--- cms/cms-app/functions/readExistingModel.mjs
+++ cms/cms-app/functions/readExistingModel.mjs
@@ -1,10 +1,17 @@
 import { action_readModel } from "../data-mgmt/actions/action_readModel.mjs";
+import { View } from "../_1_view/View.mjs";
 
 export async function readExistingModel() {
   const readModel = await action_readModel();
   console.log("readExistingModel: readModel:", readModel);
 
   // add views from state
+
+  for (const view of readModel.views) {
+    const viewDiv = await View(view);
+    document.body.appendChild(viewDiv);
+  }
+
   // add viewTemplates from state
   // add slots form state
   // add components from state
