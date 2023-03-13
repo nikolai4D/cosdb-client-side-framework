d2d9b3d1facd43d95b71fd3bd8d477ad9638817b
Nicholas Alvén <nicholas@nikolaidev.com>
2023-03-13 18:58:36 +0100

    auto commit to b

--- cms/cms-app/functions/readExistingModel.mjs
+++ cms/cms-app/functions/readExistingModel.mjs
@@ -2,6 +2,7 @@ import { action_readModel } from "../data-mgmt/actions/action_readModel.mjs";
 import { View } from "../_1_view/View.mjs";
 import { State } from "../data-mgmt/State.mjs";
 import { ViewTemplate } from "../_2_viewTemplate/ViewTemplate.mjs";
+import { createSlots } from "../_3_slot/createSlots.mjs";
 
 export async function readExistingModel() {
   const readModel = await action_readModel();
@@ -21,15 +22,17 @@ export async function readExistingModel() {
       existingViewTemplate
     );
 
-    //const ViewTemplateExistingDiv = await ViewTemplate(existingViewTemplate);
+    const ViewTemplateExistingDiv = await ViewTemplate(existingViewTemplate);
 
-    const viewDiv = await View(view, await ViewTemplate(existingViewTemplate));
+    const viewDiv = await View(view, ViewTemplateExistingDiv);
     document.body.appendChild(viewDiv);
+
+    // add slots form state
+
+    const viewTemplateBody = await getAccordionBody(id);
+    await createSlots(viewTemplateBody, viewTemplate.id, viewTemplate.value);
   }
 
-  // add viewTemplates from state
-
-  // add slots form state
   // add components from state
   // add organisms from state
   // add molecules from state
