3b49148e03a4f2c292827ee5c4bd5672ff8f5e9a
Nicholas Alvén <nicholas@nikolaidev.com>
2023-03-13 14:50:41 +0100

    auto commit to b

--- cms/cms-app/functions/readExistingModel.mjs
+++ cms/cms-app/functions/readExistingModel.mjs
@@ -1,6 +1,7 @@
 import { action_readModel } from "../data-mgmt/actions/action_readModel.mjs";
 import { View } from "../_1_view/View.mjs";
 import { State } from "../data-mgmt/State.mjs";
+import { ViewTemplate } from "../_2_viewTemplate/ViewTemplate.mjs";
 
 export async function readExistingModel() {
   const readModel = await action_readModel();
@@ -11,10 +12,13 @@ export async function readExistingModel() {
   for (const view of State.views) {
     // add viewTemplates from state
 
-    console.log(view + " view.id: " + view.id);
-
     const existingViewTemplate = State.viewTemplates.find(
       (viewTemplate) => viewTemplate.parentId === view.id
+    );
+
+    console.log(
+      "readExistingModel: existingViewTemplate:",
+      existingViewTemplate
     );
 
     const ViewTemplateExistingDiv = await ViewTemplate(existingViewTemplate);
