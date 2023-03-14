6fda39239c21467638d54780b4a02f2cdaf21e24
Nicholas Alvén <nicholas@nikolaidev.com>
2023-03-14 23:51:43 +0100

    auto commit to b

--- cms/cms-app/_6_molecule/createMolecule.mjs
+++ cms/cms-app/_6_molecule/createMolecule.mjs
@@ -84,6 +84,7 @@ async function createSubAtomsEl(subComps, id, compBody, parentBody) {
 async function createFunctionsEl(subComps, id, compBody, parentBody) {
   for (const comp of subComps) {
     const [[key, value]] = Object.entries(comp);
+    console.log("key", key, "value", value);
     const parentId = id;
 
     let childSlot = await Function(await newFunction(parentId, key), compBody);
