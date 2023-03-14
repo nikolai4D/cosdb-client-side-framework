5989556162e2dead7076862df3a5b1d6e5c6c8ef
Maja Frankiewicz <majafrank@live.se>
2023-03-14 17:43:16 +0100

    auto commit to CSFO-1-app-connect-router-to-app-and-show-components-from-model-json

--- components/molecules/Molecule_dummy1.mjs
+++ components/molecules/Molecule_dummy1.mjs
@@ -35,9 +35,11 @@ export function Molecule_dummy1() {
   `;
   }
 
-  this.bindScript= function() {
-    this.atoms.forEach(atom => {
-      this.fillSlot(atom.atom, atom.component.getElement())
-    })
+  this.bindScript= async function() {
+
+    for (let atom of this.atoms) {
+      await this.fillSlot(atom.atom, atom.component.getElement())
+    }
+
   }
 }
