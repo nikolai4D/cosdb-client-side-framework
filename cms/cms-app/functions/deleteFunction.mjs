f490c8e6a5bdf9be80c7a5336c5583a91b68d116
Nicholas Alvén <nicholas@nikolaidev.com>
2023-03-15 09:31:04 +0100

    auto commit to b

--- cms/cms-app/functions/deleteFunction.mjs
+++
@@ -1,6 +0,0 @@
-import { State } from "../data-mgmt/State.mjs";
-
-export async function deleteFunction(id) {
-  // Remove view with specified id
-  State.functions = State.functions.filter((fn) => fn.id !== id);
-}
