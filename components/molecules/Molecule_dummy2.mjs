d343948c35f8d7478e739c816267c23c7825681f
Maja Frankiewicz <majafrank@live.se>
2023-03-15 16:44:35 +0100

    auto commit to CSFO-1-app-connect-router-to-app-and-show-components-from-model-json

--- components/molecules/Molecule_dummy2.mjs
+++ components/molecules/Molecule_dummy2.mjs
@@ -1,6 +1,5 @@
 import { Component } from '../../core/Component.mjs'
 import { slot } from "../../core/helpers.mjs";
-import { Atom_dummy3 } from "../atoms/Atom_dummy3.mjs";
 import { Atom_dummy4 } from "../atoms/Atom_dummy4.mjs";
 import { State } from "../../State.mjs";
 
@@ -8,10 +7,6 @@ export function Molecule_dummy2() {
   Component.call(this);
 
   this.atoms = [
-    {
-      atom: "Atom_dummy3",
-      component: new Atom_dummy3()
-    },
     {
       atom: "Atom_dummy4",
       component: new Atom_dummy4()
@@ -28,7 +23,6 @@ export function Molecule_dummy2() {
   return `
   <div>
     <div>${slot(this.atoms[0].atom)}</div>
-    <div>${slot(this.atoms[1].atom)}</div>
   </div>
 `;
 }
