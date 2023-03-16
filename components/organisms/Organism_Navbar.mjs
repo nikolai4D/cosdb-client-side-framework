0478c88ae4d31c533c2bcf04c19b8553153a8b34
Maja Frankiewicz <majafrank@live.se>
2023-03-16 23:40:12 +0100

    auto commit to b

---
+++ components/organisms/Organism_Navbar.mjs
@@ -0,0 +1,64 @@
+import { Component } from "../../core/Component.mjs";
+import { slot } from  "../../core/helpers.mjs";
+import { Molecule_ListWHeading } from "../molecules/Molecule_ListWHeading.mjs";
+import { Molecule_HeadingSearchButton } from "../molecules/Molecule_HeadingSearchButton.mjs";
+
+export function Organism_Navbar() {
+  Component.call(this);
+
+  this.molecules = [
+    {
+        id: 1,
+        molecule: "Molecule_HeadingSearchButton",
+        component: new Molecule_HeadingSearchButton()
+    },
+    {
+        id: 2,
+        molecule: "Molecule_ListWHeading",
+        component: new Molecule_ListWHeading()
+    },
+    {
+        id: 3,
+        molecule: "Molecule_ListWHeading",
+        component: new Molecule_ListWHeading()
+    },
+    {
+        id: 4,
+        molecule: "Molecule_ListWHeading",
+        component: new Molecule_ListWHeading()
+    }
+  ];
+
+  this.functions = [
+    {
+      id: 1,
+      function: "placeholder",
+    },
+    {
+      id: 2,
+      function: "placeholder",
+    },
+  ];
+
+  this.getHtml = function(){
+
+    return `
+
+    <div class="organism_list-all-search">
+        ${slot(this.molecules[0].molecule)}
+        <div id="organism_all_lists" class="organism_list-all-search__lists">
+            ${slot(this.molecules[1].molecule)}
+            ${slot(this.molecules[2].molecule)}
+            ${slot(this.molecules[3].molecule)}
+        </div>
+    </div>
+        `;
+      }
+
+  this.bindScript= async function() {
+
+    for (let mol of this.molecules) {
+      await this.fillSlot(mol.molecule, mol.component.getElement())
+    }
+  }
+}
