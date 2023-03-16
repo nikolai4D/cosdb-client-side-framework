4e3048fa29adf9c696ef0b2edafcd1ffd00e8bf5
Maja Frankiewicz <majafrank@live.se>
2023-03-16 22:21:59 +0100

    auto commit to b

---
+++ components/atoms/Atom_Heading2.mjs
@@ -0,0 +1,19 @@
+import { Component } from "../../core/Component.mjs";
+
+
+export function Atom_Heading2() {
+  Component.call(this);
+
+  this.value = [{ value: "placeholder" }];
+
+
+  this.getHtml = function(){
+
+    return `  
+      <h4 class="atom_heading4">${this.value[0].value}</h4>
+
+    `;
+    }
+
+
+}
