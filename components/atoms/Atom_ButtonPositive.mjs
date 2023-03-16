f7a99cdff0a1585d1369866af7be25877f662ebe
Maja Frankiewicz <majafrank@live.se>
2023-03-16 22:12:30 +0100

    auto commit to b

--- components/atoms/Atom_ButtonPositive.mjs
+++ components/atoms/Atom_ButtonPositive.mjs
@@ -1,15 +1,16 @@
 import { Component } from "../../core/Component.mjs";
 
 
-export function Atom_ButtonPositive() {
+export function Atom_ButtonPositive({onClick = () => console.log("click")}) {
   Component.call(this);
 
   this.value = [{ value: "placeholder" }];
 
   this.getHtml = function(){
-    return `<li>
-                ${this.value[0].value}
-            </li>
-            `;
+    return ` <button class="atom_button-positive">${this.value[0].value}</button> `;
+    }
+
+    this.bindScript= function() {
+        this.element.addEventListener("click", onClick)
     }
 }
