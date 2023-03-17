b3abe31e546cdd901a67a2d40c726ecac5e96080
Maja Frankiewicz <majafrank@live.se>
2023-03-17 10:25:12 +0100

    auto commit to b

--- components/molecules/Molecule_ListWHeading.mjs
+++ components/molecules/Molecule_ListWHeading.mjs
@@ -106,6 +106,7 @@ export function Molecule_ListWHeading() {
     for (let func of this.functions) {
       if (await func.functionCall){
         let data = await func.functionCall();
+        console.log(data, "data")
         let dataMap = data.map((item) => {
           return item.title
         }
