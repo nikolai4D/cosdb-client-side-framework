177a8cf56faa42fb85659270d96126afbaeae356
Maja Frankiewicz <majafrank@live.se>
2023-03-17 10:21:28 +0100

    auto commit to b

--- data-mgmt/actions/GET_ALL_PROCESSES.mjs
+++ data-mgmt/actions/GET_ALL_PROCESSES.mjs
@@ -1,12 +1,13 @@
-// import {API_KEY} from '../constants.js';
+import {API_KEY} from '../constants.js';
 
 export const GET_ALL_PROCESSES = async () => {
-        const response = await fetch('https://jsonplaceholder.typicode.com/todos/1', {
+        const response = await fetch('https://uc1.cosdb.io/api/typeData?parentId=co_140ca73c-1275-4fa5-8e74-fa71e845afe3', {
           method: 'get',
         //   body: myBody, // string or object
           headers: {
-            'Content-Type': 'application/json'
-          }
+            'Content-Type': 'application/json',
+            'authorization': `${API_KEY}`
+          },
         });
         const myJson = await response.json();
         console.log(myJson);
