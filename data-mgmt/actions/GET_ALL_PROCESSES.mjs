a43776d91a482573b820e881982c3a65d6af51f2
Maja Frankiewicz <majafrank@live.se>
2023-03-17 09:43:11 +0100

    auto commit to b

--- data-mgmt/actions/GET_ALL_PROCESSES.mjs
+++ data-mgmt/actions/GET_ALL_PROCESSES.mjs
@@ -1,7 +1,6 @@
 // import {API_KEY} from '../constants.js';
 
 export const GET_ALL_PROCESSES = async () => {
-    console.log(API_KEY);
         const response = await fetch('https://jsonplaceholder.typicode.com/todos/1', {
           method: 'get',
         //   body: myBody, // string or object
