27ab970da455b384b72689fcc9a3f3d683199a93
Maja Frankiewicz <majafrank@live.se>
2023-03-21 15:40:11 +0100

    auto commit

--- data-mgmt/actions/action_getAllListData.mjs
+++ data-mgmt/actions/action_getAllListData.mjs
@@ -1,12 +1,14 @@
-import {URL_PROCESS_1, URL_PROCESS_2} from '../constants.js';
-import { apiCallGet } from './apiCalls.mjs'
+// import {URL_PROCESS_1, URL_PROCESS_2} from '../constants.js';
+// import { apiCallGet } from './apiCalls.mjs'
 
 export async function action_getAllListData() {
 
-      const firstProcess =  await apiCallGet(URL_PROCESS_1);
-      const secondProcess = await apiCallGet(URL_PROCESS_2)
+      return [{"title": test}]
 
-      return [...firstProcess, ...secondProcess]
+      // const firstProcess =  await apiCallGet(URL_PROCESS_1);
+      // const secondProcess = await apiCallGet(URL_PROCESS_2)
+
+      // return [...firstProcess, ...secondProcess]
 }
 
 // export const actions = {
