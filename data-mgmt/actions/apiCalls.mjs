ad54b05ac7119600b4bd0ce32142b5867163433e
Maja <majafrank@live.se>
2023-03-21 16:48:36 +0100

    Update apiCalls.mjs

--- data-mgmt/actions/apiCalls.mjs
+++ data-mgmt/actions/apiCalls.mjs
@@ -1,7 +1,6 @@
 export async function apiCallGet(url) {
     const response = await fetch(url, {
       method: 'get',
-      //   body: myBody, // string or object
       headers: {
         'Content-Type': 'application/json'
             },
