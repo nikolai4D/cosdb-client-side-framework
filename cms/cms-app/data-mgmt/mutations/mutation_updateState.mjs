73b8b276061c0f22708d846e82dbb6240a98cec8
Nicholas Alvén <nicholas@nikolaidev.com>
2023-03-21 22:05:08 +0100

    auto commit

--- cms/cms-app/data-mgmt/mutations/mutation_updateState.mjs
+++ cms/cms-app/data-mgmt/mutations/mutation_updateState.mjs
@@ -20,21 +20,27 @@ export async function mutation_updateState(
         console.log("data.value === ''", data.value);
         customTypeData.splice(index, 1);
       } else {
+        console.log("1");
         customTypeData.splice(index, 1, data);
       }
     }
     if (customType === "views") {
+      console.log("2");
       data.valueDisabled = customTypeData[index].valueDisabled;
       data.protected = customTypeData[index].protected;
       data.startView = customTypeData[index].startView;
 
       customTypeData.splice(index, 1, data);
     } else {
+      console.log("3");
       customTypeData.splice(index, 1, data);
     }
   } else {
+    console.log("4");
     customTypeData.push(data);
   }
+
+  console.log(await customTypeData);
 
   State[customType] = await customTypeData;
   return State;
