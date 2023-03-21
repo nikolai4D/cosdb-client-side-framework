f3dc6bdb0618acb2a29dfbe221172527d063507c
Nicholas Alvén <nicholas@nikolaidev.com>
2023-03-21 22:50:54 +0100

    auto commit

--- cms/cms-app/functions/eventChangeInput.mjs
+++ cms/cms-app/functions/eventChangeInput.mjs
@@ -32,7 +32,7 @@ export async function eventChangeInput(id) {
     data.customType = parentCustomType;
     if (value.startsWith("{")) {
       value = JSON.parse(value);
-      input.value = value;
+      input.value = JSON.stringify(value);
     }
     data.parameters = value;
 
