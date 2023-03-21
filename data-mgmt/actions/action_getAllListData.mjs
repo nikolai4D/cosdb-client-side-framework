5263c7dbc5e7ca5ffbc9d91ca4cb67b7626014d2
Maja Frankiewicz <majafrank@live.se>
2023-03-21 16:18:50 +0100

    auto commit

--- data-mgmt/actions/action_getAllListData.mjs
+++ data-mgmt/actions/action_getAllListData.mjs
@@ -3,10 +3,12 @@ import { apiCallGet } from './apiCalls.mjs'
 
 export async function action_getAllListData() {
 
-      const data =  await apiCallGet("api/");
-      console.log(data.json())
+      const response =  await apiCallGet("api/");
+      const data = await response.json();
 
-      return [{"title": data.json()}]
+      console.log(data)
+
+      return [{"title": data}]
 
       // const firstProcess =  await apiCallGet(URL_PROCESS_1);
       // const secondProcess = await apiCallGet(URL_PROCESS_2)
