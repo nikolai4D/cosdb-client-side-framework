b52a6feb84ba05c765d904e15138e5d4aee45d90
Nicholas Alvén <nicholas@nikolaidev.com>
2023-03-21 21:19:40 +0100

    auto commit

--- cms/cms-app/functions/eventChangeInput.mjs
+++ cms/cms-app/functions/eventChangeInput.mjs
@@ -3,6 +3,7 @@ import { action_writeModel } from "../data-mgmt/actions/action_writeModel.mjs";
 
 export async function eventChangeInput(id) {
   const input = document.getElementById(id);
+  console.log({ input });
   let value = input.value;
   const customType = input.getAttribute("customType");
   const parentId = input.getAttribute("parentId");
@@ -40,6 +41,7 @@ export async function eventChangeInput(id) {
   const customTypeArray = customType + "s";
 
   const state = await mutation_updateState(customTypeArray, data);
+  console.log({ state });
 
   await action_writeModel(state);
 }
