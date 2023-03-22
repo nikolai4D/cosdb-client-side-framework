c3e57795d9091a6592725c79b0e582d7595ad76a
Maja Frankiewicz <majafrank@live.se>
2023-03-22 14:05:49 +0100

    auto commit

--- data-mgmt/etl/transformer_groupByFirstLetter.mjs
+++ data-mgmt/etl/transformer_groupByFirstLetter.mjs
@@ -1,7 +1,5 @@
 export async function transformer_groupByFirstLetter(strings) {
 
-        let awaitedStrings =  strings;
-        console.log(awaitedStrings)
         const grouped = awaitedStrings.reduce((acc, str) => {
           const firstLetter = str[0].toUpperCase();
           if (!acc[firstLetter]) {
