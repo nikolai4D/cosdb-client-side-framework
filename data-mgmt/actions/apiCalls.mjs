8776d26659839fe4487a54e69b83755bae6d7af7
Nicholas Alvén <nicholas@nikolaidev.com>
2023-03-23 11:27:12 +0100

    auto commit

--- data-mgmt/actions/apiCalls.mjs
+++ data-mgmt/actions/apiCalls.mjs
@@ -1,24 +1,22 @@
 export async function apiCallGet(url) {
-    const response = await fetch(url, {
-      method: 'get',
-      headers: {
-        'Content-Type': 'application/json'
-            },
-    });
-    return await response.json();
-  }
+  const response = await fetch(url, {
+    method: "get",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  return await JSON.parse(response);
+}
 
-
-export async function apiCallPost({url, body}) {
-    const response = await fetch(url, {
-      method: 'post',
-      body,
-      headers: {
-        'Content-Type': 'application/json'
-            },
-      credentials: "include",
-      body: JSON.stringify({ body })
-    });
-    return await response.json();
-  }
-  
+export async function apiCallPost({ url, body }) {
+  const response = await fetch(url, {
+    method: "post",
+    body,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
+    body: JSON.stringify({ body }),
+  });
+  return await response.json();
+}
