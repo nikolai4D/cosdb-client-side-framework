9ae843c92f3d2f845b0d1aa66a8375722a0277d0
Nicholas Alvén <nicholas@nikolaidev.com>
2023-03-24 00:41:54 +0100

    auto commit

--- components/viewTemplates/ViewTemplate_dummy2.mjs
+++ components/viewTemplates/ViewTemplate_dummy2.mjs
@@ -19,6 +19,7 @@ export function ViewTemplate_dummy2() {
     return await html2dom`
       <div>${slot("slot3")}</div>
       <div>${slot("slot4")}</div>
+      <div>${slot("slot4")}</div>
       <div>ViewTemplate_dummy2 text</div>
     `;
   };
