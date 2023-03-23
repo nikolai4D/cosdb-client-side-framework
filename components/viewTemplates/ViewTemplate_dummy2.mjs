124c7da9452d7698f1781ff712d6f3ac5a1690a1
Nicholas Alvén <nicholas@nikolaidev.com>
2023-03-23 23:16:27 +0100

    auto commit

--- components/viewTemplates/ViewTemplate_dummy2.mjs
+++ components/viewTemplates/ViewTemplate_dummy2.mjs
@@ -2,26 +2,14 @@ import { Component } from "../../core/Component.mjs";
 import { html2dom } from "../../core/helpers.mjs";
 
 export async function ViewTemplate_dummy2() {
-  //   Component.call(this);
-
-  //   this.slots = [
-  //     {
-  //       slot: "slot3",
-  //     },
-  //     {
-  //       slot: "slot4",
-  //     },
-  //   ];
-
-  //   this.getHtml = function(){
-  //   return `
-  //     <div>
-  //           <div>${this.slots[0].slot}</div>
-  //           <div>${this.slots[1].slot}</div>
-  //     </div>
-  //   `;
-  //   }
-  // }
+  this.slots = [
+    {
+      slot: "slot3",
+    },
+    {
+      slot: "slot4",
+    },
+  ];
 
   const divDOM1 = document.createElement("div");
   const h1Element1 = document.createElement("h1");
@@ -29,9 +17,9 @@ export async function ViewTemplate_dummy2() {
   divDOM1.appendChild(h1Element1);
 
   const divDOM2 = document.createElement("div");
-  const h1Element2 = document.createElement("h1");
-  h1Element2.textContent = "2 dom";
-  divDOM1.appendChild(h1Element2);
+  const h2Element2 = document.createElement("h2");
+  h2Element2.textContent = "2 dom";
+  divDOM1.appendChild(h2Element2);
 
   return await html2dom`
 <div>${divDOM1}</div>
--- core/ViewTemplate.mjs
+++ core/ViewTemplate.mjs
@@ -9,17 +9,10 @@ export async function ViewTemplate(parentId) {
   const id = viewTemplate[0].id;
   const value = viewTemplate[0].value;
 
-  //   // Create a new div from type
-  //   const div = document.createElement("div");
-  //   div.classList.add(type);
-  //   div.setAttribute("id", id);
-  //   const h1Element = document.createElement("h1");
-  //   h1Element.textContent = value;
-  //   div.appendChild(h1Element);
-
   // get the viewTemplate
 
   const viewTemplateObject = await createComponent(type, value);
+  console.log(viewTemplateObject.slots, "viewTemplateObject.slots");
 
   return viewTemplateObject;
 }
