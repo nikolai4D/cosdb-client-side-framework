8b223f2efb66f4f95432699f78852937aaefc7fd
Nicholas Alvén <nicholas@nikolaidev.com>
2023-03-23 08:56:03 +0100

    auto commit

--- app-boilerplate/index.mjs
+++ app-boilerplate/index.mjs
@@ -2,7 +2,7 @@ import { Router } from "../core/Router.mjs";
 
 async function app() {
   const path = window.location.pathname.slice(1);
-  await new Router(path);
+  await Router(path);
 }
 
 app();
