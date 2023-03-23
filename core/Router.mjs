68c3eb90a000b1c3641d183428e51ddf251784e4
Nicholas Alvén <nicholas@nikolaidev.com>
2023-03-23 15:14:20 +0100

    auto commit

--- core/Router.mjs
+++ core/Router.mjs
@@ -5,25 +5,25 @@ export async function Router(viewPath) {
   //validate and authenticate path
   const newView = await apiCallGet(`/auth/${viewPath}`);
   console.log("newView", newView);
-  //const newPath = newView.viewPath;
+  const newPath = newView.value;
 
   //set browser history
-  //window.history.pushState({ viewPath: newPath }, "", newPath);
+  window.history.pushState({ viewPath: newPath }, "", newPath);
 
   //set view
-  //await View(newPath);
+  await View(newPath);
 }
 
 // Listen for back or forward navigation
-// window.addEventListener("popstate", async (event) => {
-//   if (event.state && event.state.viewPath) {
-//     const viewPath = event.state.viewPath;
+window.addEventListener("popstate", async (event) => {
+  if (event.state && event.state.viewPath) {
+    const viewPath = event.state.viewPath;
 
-//     //validate and authenticate path
-//     const newView = await apiCallGet(`/auth/${viewPath}`);
-//     const newPath = newView.viewPath;
+    //validate and authenticate path
+    const newView = await apiCallGet(`/auth/${viewPath}`);
+    const newPath = newView.viewPath;
 
-//     //set view
-//     await View(newPath);
-//   }
-// });
+    //set view
+    await View(newPath);
+  }
+});
