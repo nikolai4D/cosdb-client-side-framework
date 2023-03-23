aec2bd431cad626cf9839ae35c42bc92d6b2389e
Nicholas Alvén <nicholas@nikolaidev.com>
2023-03-23 07:14:24 +0100

    auto commit

--- app-boilerplate/index.mjs
+++ app-boilerplate/index.mjs
@@ -1,16 +1,8 @@
-import {Router, route} from "../core/Router.mjs";
-import {Controller} from "./helpers/Controller.mjs";
-import { readModel } from "./helpers/readModel.mjs";
-import { ViewTemplate_dummy1 } from "../../components/viewTemplates/ViewTemplate_dummy1.mjs";
+import { Router } from "../core/Router.mjs";
 
-const path = window.location.pathname.slice(1)
-console.log("path: " , path)
+async function app() {
+  const path = window.location.pathname.slice(1);
+  await new Router(path);
+}
 
-readModel();
-
-export const router = new Router([
-    route(path, Controller)
-])
-
-
-router.goTo(path)
+app();
