b3031555df20b5176ffac6d23ee4f90c6e1f7b00
Nicholas Alvén <nicholas@nikolaidev.com>
2023-03-23 15:09:36 +0100

    auto commit

--- core/View.mjs
+++ core/View.mjs
@@ -8,51 +8,31 @@ export async function View(viewPath) {
 
   // Create a new <h1> element
   const h1Element = document.createElement("h1");
+  h1Element.textContent = viewPath;
+  divElement.appendChild(h1Element);
 
-  // Create a new input field
+  // Create a new <input> element
   const inputElement = document.createElement("input");
-
-  // Set the input type to text
   inputElement.setAttribute("type", "text");
-
-  // Create a new button
-  const buttonElement = document.createElement("button");
-
-  // Set the button text to "Go"
-  buttonElement.textContent = "Go";
-
-  // Add a click event listener to the button
-  buttonElement.addEventListener("click", async function (event) {
-    // Prevent the default behavior of the button (submitting a form)
-    event.preventDefault();
-
-    // Get the input value and set the window location to it
-    const inputValue = inputElement.value;
-    await Router(inputValue);
-  });
-
-  // Add a keydown event listener to the input field
   inputElement.addEventListener("keydown", function (event) {
-    // Check if the pressed key is Enter
     if (event.key === "Enter") {
-      // Programmatically trigger a click event on the button
       buttonElement.click();
     }
   });
-
-  // Set the text of the <h1> element to the input value
-  h1Element.textContent = viewPath;
-
-  // Append the <h1> element to the <div> element
-  divElement.appendChild(h1Element);
-
-  // Append the input field and button to the <div> element
   divElement.appendChild(inputElement);
+
+  // Create a new <button> element
+  const buttonElement = document.createElement("button");
+  buttonElement.textContent = "Go";
+  buttonElement.addEventListener("click", async function (event) {
+    event.preventDefault();
+    const inputValue = inputElement.value;
+    await Router(inputValue);
+  });
   divElement.appendChild(buttonElement);
 
+  // Append the <div> element to the <body> element
   document.body.appendChild(divElement);
-
-  //   return divElement;
 }
 
 function deletePreviousView() {
