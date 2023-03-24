b75e0ee85930fdd07e0a9049eb90f3c13b354949
Nicholas Alvén <nicholas@nikolaidev.com>
2023-03-24 10:51:56 +0100

    auto commit

--- core/Slots.mjs
+++ core/Slots.mjs
@@ -1,21 +1,18 @@
 import { apiCallGet } from "../data-mgmt/actions/apiCalls.mjs";
 
 export async function Slots(parentId) {
-  const type = "slots";
+  const type = "slot";
   //validate and authenticate path
-  const slots = await apiCallGet(`/read/${type}s/${parentId}`);
-  console.log(slots);
+  const Slots = await apiCallGet(`/read/${type}s/${parentId}`);
+  console.log(Slots);
 
-  const id = viewTemplate[0].id;
-  const value = viewTemplate[0].value;
+  const SlotsArray = [];
 
-  // Create a new div from type
-  const div = document.createElement("div");
-  div.classList.add(type);
-  div.setAttribute("id", id);
-  const h1Element = document.createElement("h1");
-  h1Element.textContent = value;
-  div.appendChild(h1Element);
+  for (const slot of Slots) {
+    const id = slot.id;
+    const value = slot.value;
+    SlotsArray.push({ id: id, slot: value, content: value + " content" });
+  }
 
-  return div;
+  return SlotsArray;
 }
