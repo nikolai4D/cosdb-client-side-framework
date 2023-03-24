e062e823fe58f2255c2dcecbbe48f96333c5572a
Nicholas Alvén <nicholas@nikolaidev.com>
2023-03-24 01:52:20 +0100

    auto commit

--- components/viewTemplates/ViewTemplate_dummy2.mjs
+++ components/viewTemplates/ViewTemplate_dummy2.mjs
@@ -18,26 +18,3 @@ export function ViewTemplate_dummy2() {
     `;
   };
 }
-
-// import { html2dom } from "../../core/helpers.mjs";
-
-// export async function ViewTemplate_dummy2() {
-//   this.slots = [
-//     {
-//       slot: "slot3",
-//     },
-//     {
-//       slot: "slot4",
-//     },
-//   ];
-
-//   const slot = (slotName) => {
-//     const foundSlot = this.slots.find((s) => s.slot === slotName);
-//     return foundSlot ? foundSlot.content : "";
-//   };
-
-//   return await html2dom`
-//   <div>${slot("slot3")}</div>
-//   <div>${slot("slot4")}</div>
-// `;
-// }
