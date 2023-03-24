5173ca1e04729f3940a6629c26b28107f7fb9a29
Nicholas Alvén <nicholas@nikolaidev.com>
2023-03-24 03:05:52 +0100

    auto commit

--- components/viewTemplates/ViewTemplate_dummy2.mjs
+++ components/viewTemplates/ViewTemplate_dummy2.mjs
@@ -14,9 +14,9 @@ export function ViewTemplate_dummy2() {
     console.log(this.slots[0].slot, "this.slots[0].slot");
     console.log(this.slots[1].slot, "this.slots[1].slot");
     return await html2dom`
-        <div>${this.slots[0].slot}</div>
-        <div>${this.slots[1].slot}</div>
-        <div>TEXT</div>`;
+        <div><h1>${this.slots[0].slot}</h1></div>
+        <div><h3>${this.slots[1].slot}</h3></div>
+        <div><h5>TEXT</h5></div>`;
   };
 
   this.render = async () => {
