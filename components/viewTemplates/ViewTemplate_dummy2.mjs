ce9897d0c9a6d3965053c4080379b79c7e566539
Nicholas Alvén <nicholas@nikolaidev.com>
2023-03-24 03:27:43 +0100

    auto commit

--- components/viewTemplates/ViewTemplate_dummy2.mjs
+++ components/viewTemplates/ViewTemplate_dummy2.mjs
@@ -17,15 +17,32 @@ export function ViewTemplate_dummy2() {
     return foundSlot ? foundSlot.content : "";
   };
 
-  const comp = async () => {
-    return await html2dom`
-        <div><h1>${slt("slot3")}</h1></div>
-        <div><h3>${slt("slot4")}</h3></div>
-        <div><h5>${slt("slot3")}</h5></div>
-        <div><h5>TEXT</h5></div>`;
-  };
+  //   const comp = async () => {
+  //     return await html2dom`
+  //         <div><h1>${slt("slot3")}</h1></div>
+  //         <div><h3>${slt("slot4")}</h3></div>
+  //         <div><h5>${slt("slot3")}</h5></div>
+  //         <div><h5>TEXT</h5></div>`;
+  //   };
 
   this.render = async () => {
-    return await comp();
+    return await html2dom`
+    <div><h1>${slt("slot3")}</h1></div>
+    <div><h3>${slt("slot4")}</h3></div>
+    <div><h5>${slt("slot3")}</h5></div>
+    <div><h5>TEXT</h5></div>`;
   };
 }
+
+// const comp = async () => {
+//     return await html2dom`
+//         <div><h1>${slt("slot3")}</h1></div>
+//         <div><h3>${slt("slot4")}</h3></div>
+//         <div><h5>${slt("slot3")}</h5></div>
+//         <div><h5>TEXT</h5></div>`;
+//   };
+
+//   this.render = async () => {
+//     return await comp();
+//   };
+// }
