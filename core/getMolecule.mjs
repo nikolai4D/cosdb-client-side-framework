3c203b9f720924444bdbf5d1a18c23c6af7c8a02
Nicholas Alvén <nicholas@nikolaidev.com>
2023-03-25 22:38:01 +0100

    auto commit

--- core/getMolecule.mjs
+++ core/getMolecule.mjs
@@ -29,7 +29,7 @@ export async function getMolecule(module, parentId) {
     atomsObject.push({
       id: atomId,
       value: value,
-      component: await atomObject[0],
+      component: await atomObject.render(),
     });
   }
 
