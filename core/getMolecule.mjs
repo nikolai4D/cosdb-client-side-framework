302ba8aa2f9dd05b5bce3b411638aa237c8f0ac1
Nicholas Alvén <nicholas@nikolaidev.com>
2023-03-25 22:08:15 +0100

    auto commit

--- core/getMolecule.mjs
+++ core/getMolecule.mjs
@@ -1,6 +1,7 @@
 import { apiCallGet } from "../data-mgmt/actions/apiCalls.mjs";
 import { getAtom } from "./getAtom.mjs";
 import { getFunction } from "./getFunction.mjs";
+import { createComponent } from "./helpers.mjs";
 
 export async function getMolecule(module, parentId) {
   const modelMolecules = await apiCallGet(`/read/molecules`);
