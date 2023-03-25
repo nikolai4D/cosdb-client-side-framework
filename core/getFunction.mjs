32b9d6d2f78df3894ae0c75867b0ad9cd0d7c1ce
Nicholas Alvén <nicholas@nikolaidev.com>
2023-03-25 22:10:21 +0100

    auto commit

--- core/getFunction.mjs
+++ core/getFunction.mjs
@@ -1,7 +1,7 @@
 import { apiCallGet } from "../data-mgmt/actions/apiCalls.mjs";
 import { createAction } from "./helpers.mjs";
 
-export async function getFunction(parentId) {
+export async function getFunction(module, parentId) {
   const modelFunction = await apiCallGet(`/read/functions`);
 
   const func = modelFunction.filter((fn) => fn.parentId === parentId);
