4782dc0b2e938ff47d2e203640b606c6731b95f9
Nicholas Alvén <nicholas@nikolaidev.com>
2023-03-26 01:03:28 +0100

    auto commit

---
+++ components/organisms/Organism_Dev3.mjs
@@ -0,0 +1,80 @@
+import { Molecule_Dev1 } from "../molecules/Molecule_Dev1.mjs";
+import { Molecule_Dev2 } from "../molecules/Molecule_Dev2.mjs";
+import { Organism_Dev1 } from "./Organism_Dev1.mjs";
+import { Organism_Dev2 } from "./Organism_Dev2.mjs";
+import { html2dom } from "../../core/helpers.mjs";
+
+export function Organism_Dev3() {
+  this.organisms = [
+    {
+      id: 1,
+      organism: "Organism_Dev1",
+      component: new Organism_Dev1(),
+    },
+    {
+      id: 2,
+      organism: "Organism_Dev2",
+      component: new Organism_Dev2(),
+    },
+  ];
+
+  this.molecules = [
+    {
+      id: 1,
+      molecule: "Molecule_Dev1",
+      component: new Molecule_Dev1(),
+    },
+    {
+      id: 2,
+      molecule: "Molecule_Dev2",
+      component: new Molecule_Dev2(),
+    },
+  ];
+
+  this.functions = [
+    {
+      id: 1,
+      function: function placeholder1() {
+        console.log("placeholder1");
+      },
+    },
+    {
+      id: 2,
+      function: function placeholder2() {
+        console.log("placeholder2");
+      },
+    },
+  ];
+
+  const fn = (id) =>
+    this.functions.find((fn) => fn.id === id)?.function() || "";
+  fn(1);
+  fn(2);
+
+  const childOrganism = async (id) => {
+    const component = Array.from(
+      this.organisms.find((childOrg) => childOrg.id === id)?.component
+    );
+    return component.map((elem) => elem.outerHTML).join("");
+  };
+
+  const molecule = async (id) => {
+    const component = Array.from(
+      this.molecules.find((mol) => mol.id === id)?.component
+    );
+    return component.map((elem) => elem.outerHTML).join("");
+  };
+
+  const organism = async () => {
+    return await html2dom`
+    <div class="organism_dev2">
+    <div>${await childOrganism(1)}</div>
+    <div>${await molecule(1)}</div>
+    <div>${await molecule(2)}</div>
+  </div>`;
+  };
+
+  this.render = async () => {
+    return await organism();
+  };
+}
--- core/getOrganism.mjs
+++ core/getOrganism.mjs
@@ -1,23 +1,95 @@
+// import { apiCallGet } from "../data-mgmt/actions/apiCalls.mjs";
+// import { getMolecule } from "./getMolecule.mjs";
+// import { getFunction } from "./getFunction.mjs";
+// import { createComponent } from "./helpers.mjs";
+
+// export async function getOrganism(module, parentId) {
+//   const modelOrganisms = await apiCallGet(`/read/organisms`);
+//   const modelMolecules = await apiCallGet(`/read/molecules`);
+
+//   const modelFunctions = await apiCallGet(`/read/functions`);
+
+//   const type = "organism";
+
+//   const organism = modelOrganisms.filter(
+//     (organism) => organism.parentId === parentId
+//   );
+//   const organismId = organism[0].id;
+
+//   //childOrganisms
+//   ??? an organism could have child organisms. Call getOrganism recursively but how?
+
+//   //molecules
+//   const moleculesObject = [];
+//   const molecules = modelMolecules.filter(
+//     (molecule) => molecule.parentId === organismId
+//   );
+
+//   for (const molecule of molecules) {
+//     const value = molecule.value;
+//     const moleculeObject = await getMolecule(value, organismId, molecule.id);
+
+//     const moleculeId = parseInt(molecule.key.split(" ")[1]);
+//     moleculesObject.push({
+//       id: moleculeId,
+//       value: value,
+//       component: moleculeObject,
+//     });
+//   }
+
+//   //functions
+//   const functionsObject = [];
+//   const funcs = modelFunctions.filter(
+//     (func) => func.parentId === molecule[0].id
+//   );
+//   for (const func of funcs) {
+//     const value = func.value;
+//     const funcObject = await getFunction(value, moleculeId);
+//     const funcId = parseInt(func.key.split(" ")[1]);
+//     functionsObject.push({
+//       id: funcId,
+//       value: value,
+//       function: await funcObject,
+//     });
+//   }
+
+//   const organismObject = await createComponent(type, module);
+//   organismObject.molecules = moleculesObject;
+//   organismObject.functions = functionsObject;
+
+//   organismObject.organisms = ??? if there are child organisms, add them here
+
+//   const renderOrganism = await organismObject.render();
+
+//   return await renderOrganism;
+// }
+
 import { apiCallGet } from "../data-mgmt/actions/apiCalls.mjs";
 import { getMolecule } from "./getMolecule.mjs";
 import { getFunction } from "./getFunction.mjs";
 import { createComponent } from "./helpers.mjs";
 
 export async function getOrganism(module, parentId) {
-  const modelOrganims = await apiCallGet(`/read/organisms`);
+  const modelOrganisms = await apiCallGet(`/read/organisms`);
   const modelMolecules = await apiCallGet(`/read/molecules`);
-
   const modelFunctions = await apiCallGet(`/read/functions`);
 
   const type = "organism";
 
-  const organism = modelOrganims.filter(
+  const organism = modelOrganisms.filter(
     (organism) => organism.parentId === parentId
   );
   const organismId = organism[0].id;
 
   //childOrganisms
-  //await getOrganism(module, moleculeId);
+  const childOrganisms = modelOrganisms.filter(
+    (organism) => organism.parentId === organismId
+  );
+  const childOrganismsObjects = [];
+  for (const childOrganism of childOrganisms) {
+    const childOrganismObject = await getOrganism(module, childOrganism.id);
+    childOrganismsObjects.push(childOrganismObject);
+  }
 
   //molecules
   const moleculesObject = [];
@@ -56,6 +128,10 @@ export async function getOrganism(module, parentId) {
   const organismObject = await createComponent(type, module);
   organismObject.molecules = moleculesObject;
   organismObject.functions = functionsObject;
+
+  if (childOrganismsObjects.length > 0) {
+    organismObject.organisms = childOrganismsObjects; // Assign the child organisms only if there are any
+  }
 
   const renderOrganism = await organismObject.render();
 
