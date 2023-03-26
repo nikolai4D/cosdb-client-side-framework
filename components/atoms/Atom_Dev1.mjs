f5c342b6e2c1668997f0082d5c8a70b01ca27a6c
Nicholas Alvén <nicholas@nikolaidev.com>
2023-03-26 20:20:54 +0200

    auto commit

--- components/atoms/Atom_Dev1.mjs
+++ components/atoms/Atom_Dev1.mjs
@@ -1,34 +1,12 @@
-// import { html2dom } from "../../core/helpers.mjs";
-// import { Atom } from "../../core/Atom.mjs";
-
-// export function Atom_Dev1() {
-//   Atom.call(this);
-
-//   const component = async () => {
-//     return await html2dom`
-//           <button>${await this.atom()}</button>`;
-//   };
-
-//   this.render = async () => {
-//     return await component();
-//   };
-// }
-
+import { html2dom } from "../../core/helpers.mjs";
 import { Atom } from "../../core/Atom.mjs";
 
 export function Atom_Dev1() {
   Atom.call(this);
 
   const component = async () => {
-    const template = `<button>${await this.atom()}</button>`;
-    const processedTemplate = await this.processTemplateWithResolver(template);
-
-    // Add event listener to the button
-    processedTemplate.addEventListener("click", () => {
-      console.log("Button clicked");
-    });
-
-    return processedTemplate;
+    return await html2dom`
+          <button>${await this.atom()}</button>`;
   };
 
   this.render = async () => {
