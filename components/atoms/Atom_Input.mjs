4440d94b5e5a223a2e046d37bc77b49d15e4f14a
Maja Frankiewicz <majafrank@live.se>
2023-03-26 11:01:21 +0200

    auto commit

--- components/atoms/Atom_Input.mjs
+++ components/atoms/Atom_Input.mjs
@@ -6,7 +6,13 @@ export function Atom_Input() {
 
   this.value = [{ value: "placeholder" }];
 
+  this.change = (e) => console.log(e.target.value)
+  
   this.getHtml = function(){
     return `<input class="atom_input" type="text" placeholder="${this.value[0].value}">`;
     }
+
+  this.bindScript= function() {
+      this.element.addEventListener("change", this.change)
+  }
 }
