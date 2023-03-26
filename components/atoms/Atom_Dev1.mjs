538a519cea10eba950292440d6aeb893425e21db
Nicholas Alvén <nicholas@nikolaidev.com>
2023-03-26 14:25:29 +0200

    auto commit

--- components/atoms/Atom_Dev1.mjs
+++ components/atoms/Atom_Dev1.mjs
@@ -4,9 +4,9 @@ import { Atom } from "../../core/Atom.mjs";
 export function Atom_Dev1() {
   Atom.call(this);
 
-  this.value = [{ value: "placeholder" }];
+  //   this.value = [{ value: "placeholder" }];
 
-  const atom = async () => this.value[0].value;
+  //   const atom = async () => this.value[0].value;
 
   const component = async () => {
     return await html2dom`
--- core/Atom.mjs
+++ core/Atom.mjs
@@ -1,3 +1,9 @@
 export function Atom() {
   this.title = "Atom";
+
+  this.value = [{ value: "placeholder" }];
+
+  this.atom = async function () {
+    return this.value[0].value;
+  };
 }
