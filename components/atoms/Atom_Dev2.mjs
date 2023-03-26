bca5dc14a0d5c9ffd3a3f0c26f5a37206a21db20
Nicholas Alvén <nicholas@nikolaidev.com>
2023-03-26 11:00:05 +0200

    auto commit

--- components/atoms/Atom_Dev2.mjs
+++ components/atoms/Atom_Dev2.mjs
@@ -3,11 +3,11 @@ import { html2dom } from "../../core/helpers.mjs";
 export function Atom_Dev2() {
   this.value = [{ value: "placeholder" }];
 
-  const atom = () => this.value[0].value;
+  const atom = async () => this.value[0].value;
 
   const viewTemplate = async () => {
     return await html2dom`
-    <p "class="atom_text3">${atom()}</p>`;
+    <p "class="atom_text3">${await atom()}</p>`;
   };
 
   this.render = async () => {
