ed04578f609d2d5d427f07dc2c9cc3d078faea24
Nicholas Alvén <nicholas@nikolaidev.com>
2023-03-26 14:18:29 +0200

    auto commit

--- components/atoms/Atom_Dev1.mjs
+++ components/atoms/Atom_Dev1.mjs
@@ -8,33 +8,12 @@ export function Atom_Dev1() {
 
   const atom = async () => this.value[0].value;
 
-  //   const component = async () => {
-  //     return await html2dom`
-  //     <button>${await atom()}</button>`;
-  //   };
-
-  //   this.render = async () => {
-  //     return await component();
-  //   };
-  // }
-
   const component = async () => {
-    const nodes = await html2dom`
+    return await html2dom`
       <button>${await atom()}</button>`;
-    const container = document.createElement("div");
-    nodes.forEach((node) => container.appendChild(node));
-    return container;
   };
 
   this.render = async () => {
-    const comp = await component();
-    console.log("comp", comp);
-
-    const button = comp.querySelector("button");
-    button.addEventListener("click", () => {
-      console.log("clicked");
-    });
-
-    return comp;
+    return await component();
   };
 }
