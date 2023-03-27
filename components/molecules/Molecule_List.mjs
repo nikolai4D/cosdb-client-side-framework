e500450991269bc5cdc427ea46f51cc014d71410
Maja Frankiewicz <majafrank@live.se>
2023-03-27 16:01:59 +0200

    auto commit

--- components/molecules/Molecule_List.mjs
+++ components/molecules/Molecule_List.mjs
@@ -38,25 +38,6 @@ export function Molecule_List() {
 
       for (let atom of this.atoms) {
 
-        let component = atom.component.getElement()
-        let elementsArray =[];
-    
-        // if (elementsArray.length === 0) elementsArray = [document.getElementById("organism_all_lists")]
-          //  for(let child of elementsArray){
-          //   for (let element of child.children[1].children)
-          //   {
-            component.addEventListener("click", (e) => {
-                    const modalId = document.getElementById('modal-processView')
-                  console.log("hello")
-                    modalId.innerHTML = `
-                        <div>
-                            ${slot("new-modal")}
-                        </div>
-                        `
-                    this.fillSlot("new-modal", component);
-                })
-            // }
-          // }
 
 
 
