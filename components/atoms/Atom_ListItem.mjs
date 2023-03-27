40395fa1cc8f71bb3d6f7857a9be474697da64cf
Maja Frankiewicz <majafrank@live.se>
2023-03-27 16:03:41 +0200

    auto commit

--- components/atoms/Atom_ListItem.mjs
+++ components/atoms/Atom_ListItem.mjs
@@ -12,27 +12,4 @@ export function Atom_ListItem() {
             </li>
             `;
     }
-
-  this.bindScript = () => {
-    let component = this.element.getElement()
-    let elementsArray =[];
-
-    // if (elementsArray.length === 0) elementsArray = [document.getElementById("organism_all_lists")]
-      //  for(let child of elementsArray){
-      //   for (let element of child.children[1].children)
-      //   {
-        component.addEventListener("click", (e) => {
-                const modalId = document.getElementById('modal-processView')
-              console.log("hello")
-                modalId.innerHTML = `
-                    <div>
-                        ${slot("new-modal")}
-                    </div>
-                    `
-                this.fillSlot("new-modal", component);
-            })
-        // }
-      // }
-
-  }
 }
