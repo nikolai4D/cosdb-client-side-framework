5833a92e298e6149ce4e5617d4ba43fef1fc0e4c
Maja Frankiewicz <majafrank@live.se>
2023-03-27 09:13:44 +0200

    auto commit

--- components/organisms/Organism_ListAll.mjs
+++ components/organisms/Organism_ListAll.mjs
@@ -81,7 +81,7 @@ export function Organism_ListAll() {
     let that = this;
     for (let org of this.organisms) {
       org.component.parent = that.element;
-      let allLists = that.element.querySelector("#organism_all_lists").children.querySelectAll("li")
+      let allLists = that.element.querySelector("#organism_all_lists").children.forEach(child => console.log(child.querySelectAll("li")))
       console.log(org.component.parent, "org.component.parent");
       console.log(allLists, "allLists");
 
