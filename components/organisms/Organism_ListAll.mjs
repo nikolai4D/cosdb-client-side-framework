9bad578abd4173aa6b130ca2f92ec0f655e3dc4d
Maja Frankiewicz <majafrank@live.se>
2023-03-27 13:53:03 +0200

    auto commit

--- components/organisms/Organism_ListAll.mjs
+++ components/organisms/Organism_ListAll.mjs
@@ -174,7 +174,7 @@ export function Organism_ListAll() {
     for (const  mol of this.molecules) {
       moleculesSlots.appendChild(mol.component.getElement());
     }
-    console.log(moleculesSlots.querySelectorAll("li"), "moleculesSlots.querySelectorAll(li)")
+    console.log(moleculesSlots)
 
     
     let that = this;
