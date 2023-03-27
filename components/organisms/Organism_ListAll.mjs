e49cb5710b6e4b5dc6f8d9d7df8ea5efd561c8f4
Maja Frankiewicz <majafrank@live.se>
2023-03-27 13:08:46 +0200

    auto commit

--- components/organisms/Organism_ListAll.mjs
+++ components/organisms/Organism_ListAll.mjs
@@ -163,7 +163,7 @@ export function Organism_ListAll() {
     this.molecules= [...newMolecules]
   };
 
-  const renderMolecules = () => {
+  const renderMolecules = async () => {
     // Replacing placeholder DOM elements (slots are rendered at this point) with new molecule DOM elements 
 
    let content= document.getElementById("organism_all_lists")
@@ -175,9 +175,9 @@ export function Organism_ListAll() {
     }
     
     for (let org of this.organisms) {
-      this.fillSlot(org.organism, org.component.getElement())
+      await this.fillSlot(org.organism, org.component.getElement())
     }
-
+    
     let that = this;
 
 
@@ -196,6 +196,4 @@ export function Organism_ListAll() {
       // console.log(elementsArray, "elementsArray");
       // org.component.parent = elementsArray;
 
-
-  };
 }
