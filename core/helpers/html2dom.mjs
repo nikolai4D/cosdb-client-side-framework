a1396d5bd3cc958be527b5539bd67ecd4b41ac0f
Nicholas Alvén <nicholas@nikolaidev.com>
2023-03-30 20:10:39 +0200

    auto commit

--- core/helpers/html2dom.mjs
+++ core/helpers/html2dom.mjs
@@ -1,51 +1,80 @@
+// export async function html2dom(strings, ...values) {
+//   const container = document.createElement("div");
+//   const placeholders = [];
+
+//   let interpolatedHTML = "";
+
+//   for (const [index, string] of strings.entries()) {
+//     interpolatedHTML += string;
+
+//     if (values[index] !== undefined) {
+//       const placeholderId = `placeholder-${index}`;
+//       placeholders.push(placeholderId);
+//       interpolatedHTML += `<!--${placeholderId}-->`;
+//     }
+//   }
+
+//   container.innerHTML = interpolatedHTML;
+
+//   for (const [index, placeholderId] of placeholders.entries()) {
+//     const commentNode = findCommentNode(container, placeholderId);
+//     if (commentNode) {
+//       if (values[index] instanceof HTMLElement) {
+//         commentNode.parentNode.replaceChild(values[index], commentNode);
+//       } else {
+//         const textNode = document.createTextNode(values[index]);
+//         commentNode.parentNode.replaceChild(textNode, commentNode);
+//       }
+//     }
+//   }
+
+//   // Return the only child element when there's one child, otherwise return the wrapper element
+//   return container.children.length === 1 ? container.children[0] : container;
+// }
+
+// function findCommentNode(element, commentText) {
+//   for (const node of element.childNodes) {
+//     if (
+//       node.nodeType === Node.COMMENT_NODE &&
+//       node.textContent.trim() === commentText
+//     ) {
+//       return node;
+//     }
+
+//     if (node.childNodes.length > 0) {
+//       const result = findCommentNode(node, commentText);
+//       if (result) return result;
+//     }
+//   }
+
+//   return null;
+// }
+
 export async function html2dom(strings, ...values) {
   const container = document.createElement("div");
-  const placeholders = [];
+  let element = null;
 
-  let interpolatedHTML = "";
+  for (let i = 0; i < strings.length; i++) {
+    container.insertAdjacentHTML("beforeend", strings[i]);
 
-  for (const [index, string] of strings.entries()) {
-    interpolatedHTML += string;
-
-    if (values[index] !== undefined) {
-      const placeholderId = `placeholder-${index}`;
-      placeholders.push(placeholderId);
-      interpolatedHTML += `<!--${placeholderId}-->`;
-    }
-  }
-
-  container.innerHTML = interpolatedHTML;
-
-  for (const [index, placeholderId] of placeholders.entries()) {
-    const commentNode = findCommentNode(container, placeholderId);
-    if (commentNode) {
-      if (values[index] instanceof HTMLElement) {
-        commentNode.parentNode.replaceChild(values[index], commentNode);
+    if (i < values.length) {
+      const value = values[i];
+      if (value instanceof HTMLElement) {
+        container.appendChild(value.cloneNode(true));
       } else {
-        const textNode = document.createTextNode(values[index]);
-        commentNode.parentNode.replaceChild(textNode, commentNode);
+        container.insertAdjacentHTML("beforeend", value);
       }
     }
   }
 
-  // Return the only child element when there's one child, otherwise return the wrapper element
-  return container.children.length === 1 ? container.children[0] : container;
-}
-
-function findCommentNode(element, commentText) {
-  for (const node of element.childNodes) {
-    if (
-      node.nodeType === Node.COMMENT_NODE &&
-      node.textContent.trim() === commentText
-    ) {
-      return node;
-    }
-
-    if (node.childNodes.length > 0) {
-      const result = findCommentNode(node, commentText);
-      if (result) return result;
+  if (container.children.length === 1) {
+    element = container.firstElementChild;
+  } else {
+    element = document.createElement("div");
+    while (container.firstChild) {
+      element.appendChild(container.firstChild);
     }
   }
 
-  return null;
+  return element;
 }
