b15b3427695bab5a3a43b039c0fb5d9706249baf
Nicholas Alvén <nicholas@nikolaidev.com>
2023-03-30 17:29:17 +0200

    auto commit

--- components/molecules/Molecule_ListWHeading2.mjs
+++ components/molecules/Molecule_ListWHeading2.mjs
@@ -26,33 +26,11 @@ export function Molecule_ListWHeading2(data = null) {
     { title: "list item 3" },
   ];
 
-  //   const listItems = async (list) => {
-  //     const items = await Promise.all(
-  //       list.map(async () => {
-  //         return await this.atom(2);
-  //       })
-  //     );
-  //     return items;
-  //   };
-
-  //   const listItems = async (list) => {
-  //     const items = await Promise.all(
-  //       list.map(async (item) => {
-  //         return await this.atom(2).toString(item);
-  //       })
-  //     );
-  //     return items.join(" ");
-  //   };
-
   const component = async () => {
     const comp = await html2dom`
     <div class="molecule_list">
     <div>${await this.atom(1)}</div>
-    <div>${list
-      .map(async (item) => {
-        return await this.atom(2);
-      })
-      .join("")}</div>
+    <div>${list.map(await this.atom(2)).join("")}</div>
     </div>`;
     return comp;
   };
@@ -61,5 +39,3 @@ export function Molecule_ListWHeading2(data = null) {
     return await component();
   };
 }
-
-//<div>${await listItems(list)}</div>;
