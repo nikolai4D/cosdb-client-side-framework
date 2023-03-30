fba085910070b1ca75212b89ba3dc1b4190d4822
Maja Frankiewicz <majafrank@live.se>
2023-03-30 11:15:14 +0200

    auto commit

--- components/organisms/Organism_ModalProcessPrep.mjs
+++ components/organisms/Organism_ModalProcessPrep.mjs
@@ -61,7 +61,7 @@ export function Organism_ModalProcessPrep() {
           
             console.log(modalElement)
 
-            this.fillSlot("new-modal", modalElement);
+            // this.fillSlot("new-modal", modalElement);
         })
     }
   }
