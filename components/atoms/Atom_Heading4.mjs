3d954a4c630f0931a51ff0ee70ea8270d3077021
Nicholas Alvén <nicholas@nikolaidev.com>
2023-03-30 16:09:42 +0200

    auto commit

--- components/atoms/Atom_Heading4.mjs
+++ components/atoms/Atom_Heading4.mjs
@@ -1,19 +1,17 @@
-import { Component } from "../../core/Component.mjs";
+//import { html2dom } from "../../core/helpers.mjs";
+import { html2dom } from "../../core/helpers/html2dom.mjs";
+import { Atom } from "../../core/Atom.mjs";
 
+export function Atom_Heading4(data = "H4 data placeholder") {
+  Atom.call(this);
 
-export function Atom_Heading4() {
-  Component.call(this);
+  const component = async () => {
+    const comp = await html2dom`
+    <h4 "class="atom_heading4">${data}</h4>`;
+    return comp;
+  };
 
-  this.value = [{ value: "placeholder" }];
-
-
-  this.getHtml = function(){
-
-    return `  
-      <h4 class="atom_heading4">${this.value[0].value}</h4>
-
-    `;
-    }
-
-
+  this.render = async () => {
+    return await component();
+  };
 }
---
+++ components/molecules/Molecule_ListWHeading2.mjs
@@ -0,0 +1,47 @@
+import { Atom_ListItem2 } from "../atoms/Atom_ListItem2.mjs";
+import { Atom_Heading4 } from "../atoms/Atom_Heading4.mjs";
+import { html2dom } from "../../core/helpers/html2dom.mjs";
+import { Molecule } from "../../core/Molecule.mjs";
+
+export function Molecule_ListWHeading(data = {}) {
+  Molecule.call(this);
+  this.atoms = [
+    {
+      id: 1,
+      atom: "Atom_Heading4",
+      component: new Atom_Heading4(),
+    },
+    {
+      id: 2,
+      atom: "Atom_ListItem2",
+      component: new Atom_ListItem2(),
+    },
+  ];
+
+  this.functions = [];
+
+  const component = async () => {
+    const comp = await html2dom`
+    <div class="molecule_list">
+    <div>${await listItems(list)}</div>
+    <div></div>
+    </div>`;
+    return comp;
+  };
+
+  this.render = async () => {
+    return await component();
+  };
+}
+
+const list = [
+  { title: "list item 1" },
+  { title: "list item 2" },
+  { title: "list item 3" },
+];
+
+const listItems = async (list) => {
+  for (const item of list) {
+    await this.atom(2);
+  }
+};
