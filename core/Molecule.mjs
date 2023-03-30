0ce8e037ca38fedcb9a3b0f1452ef11b2792abc7
Nicholas Alvén <nicholas@nikolaidev.com>
2023-03-30 18:44:38 +0200

    auto commit

--- core/Molecule.mjs
+++ core/Molecule.mjs
@@ -5,12 +5,12 @@ export function Molecule() {
     return this.functions.find((fn) => fn.id === id)?.function() || "";
   };
 
-  this.atom = new (function (id) {
+  this.atom = function (id) {
     const component = this.atoms.find((atom) => atom.id === id)?.component;
 
     // return component.map((elem) => elem.outerHTML).join("");
     return component;
-  })();
+  };
 }
 
 //   this.atom = async function (id) {
