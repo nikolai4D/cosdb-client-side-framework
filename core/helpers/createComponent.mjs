48bf598ebb30ca7a49babf292ac4877061891aae
Nicholas Alvén <nicholas@nikolaidev.com>
2023-03-30 21:38:00 +0200

    auto commit

--- core/helpers/createComponent.mjs
+++ core/helpers/createComponent.mjs
@@ -2,7 +2,7 @@ import { importModuleFromFile } from "./importModuleFromFile.mjs";
 export async function createComponent(type, file) {
   const pathToComponent = `../../../components/${type}s/${file}.mjs`;
   const Component = await importModuleFromFile(pathToComponent, file);
-  //   const component = new Component[file]();
-  const component = Component[file]();
+  const component = new Component[file]();
+
   return component;
 }
