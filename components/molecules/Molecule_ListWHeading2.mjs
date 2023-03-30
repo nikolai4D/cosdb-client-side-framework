ad4aeee45ce13284f35f25470ec64590cbdfc4a3
Nicholas Alvén <nicholas@nikolaidev.com>
2023-03-30 16:13:38 +0200

    auto commit

--- components/molecules/Molecule_ListWHeading2.mjs
+++ components/molecules/Molecule_ListWHeading2.mjs
@@ -3,7 +3,7 @@ import { Atom_Heading4 } from "../atoms/Atom_Heading4.mjs";
 import { html2dom } from "../../core/helpers/html2dom.mjs";
 import { Molecule } from "../../core/Molecule.mjs";
 
-export function Molecule_ListWHeading2(data = {}) {
+export function Molecule_ListWHeading2(data = null) {
   Molecule.call(this);
   this.atoms = [
     {
