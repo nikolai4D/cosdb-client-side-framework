bf886fa57d70ce69f0c0920fd94890929ad8d779
Nicholas Alvén <nicholas@nikolaidev.com>
2023-03-30 20:50:34 +0200

    auto commit

--- components/molecules/Molecule_ListWHeading2.mjs
+++ components/molecules/Molecule_ListWHeading2.mjs
@@ -47,7 +47,7 @@ export function Molecule_ListWHeading2(data = null) {
     const comp = await html2dom`
     <div class="molecule_list">
     <div>${await this.atom(1)}</div>
-    <div><ul>${await listItems(list)}</ul></div>
+    <ul>${await listItems(list)}</ul>
     </div>`;
 
     return comp;
