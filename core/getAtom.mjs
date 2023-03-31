7656c8660dbaa5e2f92847440297adc8066fbc12
Nicholas Alvén <nicholas@nikolaidev.com>
2023-03-31 12:29:10 +0200

    auto commit

--- core/getAtom.mjs
+++ core/getAtom.mjs
@@ -18,10 +18,7 @@ export async function getAtom(module, parentId, atomId = null) {
   const atomObject = await createComponent(type, module);
   atomObject.value = [{ value: atomValue[0].value }];
 
-  //console.log("atomObject", atomObject);
-
   const renderAtom = await atomObject.render();
-  //console.log("renderAtom", renderAtom);
 
   return await renderAtom;
 }
