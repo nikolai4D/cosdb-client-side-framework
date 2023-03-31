21271d30ca0b50baf4d3c58e9bd722c13e6b2a49
Nicholas Alvén <nicholas@nikolaidev.com>
2023-03-31 11:16:22 +0200

    auto commit

--- components/atoms/Atom_ListItem2.mjs
+++ components/atoms/Atom_ListItem2.mjs
@@ -5,9 +5,24 @@ import { Atom } from "../../core/Atom.mjs";
 export function Atom_ListItem2(data = "list data placeholder") {
   Atom.call(this);
 
+  //   const component = async () => {
+  //     const comp = await html2dom`
+  //     <li class="${await this.atom()}">${data}</li>`;
+  //     return comp;
+  //   };
+
+  //   this.render = async () => {
+  //     return await component();
+  //   };
+  // }
+
   const component = async () => {
-    const comp = await html2dom`
-    <li class="${await this.atom()}">${data}</li>`;
+    const li = document.createElement("li");
+    li.classList.add(await this.atom());
+    li.textContent = data;
+
+    // const comp = await html2dom`
+    // <li class="${await this.atom()}">${data}</li>`;
     return comp;
   };
 
