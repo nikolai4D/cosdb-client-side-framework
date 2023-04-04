95a0421b65a1e7b4cc453afa69ee8bb8e3feaf85
Nicholas Alvén <nicholas@nikolaidev.com>
2023-04-04 11:03:10 +0200

    auto commit

--- components/atoms/Atom_Heading4.mjs
+++ components/atoms/Atom_Heading4.mjs
@@ -1,6 +1,7 @@
 //import { html2dom } from "../../core/helpers.mjs";
 import { html2dom } from "../../core/helpers/html2dom.mjs";
 import { Atom } from "../../core/Atom.mjs";
+import { createElement } from "../../core/helpers/createElement.mjs";
 
 export function Atom_Heading4(data = "H4 data placeholder") {
   Atom.call(this);
@@ -11,8 +12,10 @@ export function Atom_Heading4(data = "H4 data placeholder") {
   atomVal();
 
   const component = async () => {
-    const comp = await html2dom`
-    <h4 "class="atom_heading4">${data}</h4>`;
+    // const comp = await html2dom`
+    // <h4 "class="atom_heading4">${data}</h4>`;
+    // return comp;
+    const comp = await createElement("h4", { class: await this.atom() }, data);
     return comp;
   };
 
