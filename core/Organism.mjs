12671c9dbf6f2c7dbf9f69b0addc169a458c41ea
Nicholas Alvén <nicholas@nikolaidev.com>
2023-04-04 14:52:38 +0200

    auto commit

--- core/Organism.mjs
+++ core/Organism.mjs
@@ -8,6 +8,12 @@ export function Organism() {
   this.childOrganism = async (id, data) => {
     const component = this.organisms.find((mol) => mol.id === id)?.component;
     const comp = component.comp;
+    const compChildOrganisms = component.organisms;
+    const compMolecules = component.molecules;
+    const compFunctions = component.functions;
+    comp.organisms = compChildOrganisms;
+    comp.molecules = compMolecules;
+    comp.functions = compFunctions;
     const renderComp = await comp.render(data);
     return renderComp;
   };
@@ -16,6 +22,10 @@ export function Organism() {
     const component = this.molecules.find((mol) => mol.id === id)?.component;
     console.log(component);
     const comp = component.comp;
+    const compAtoms = component.atoms;
+    const compFunctions = component.functions;
+    comp.atoms = compAtoms;
+    comp.functions = compFunctions;
     const renderComp = await comp.render(data);
     return renderComp;
   };
