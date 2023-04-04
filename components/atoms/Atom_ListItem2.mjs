8e5bc6c6ad30487e0d94c3c5618176b43f6e8019
Nicholas Alvén <nicholas@nikolaidev.com>
2023-04-04 10:58:54 +0200

    auto commit

--- components/atoms/Atom_ListItem2.mjs
+++ components/atoms/Atom_ListItem2.mjs
@@ -1,13 +1,17 @@
 //import { html2dom } from "../../core/helpers.mjs";
 import { html2dom } from "../../core/helpers/html2dom.mjs";
+import { createElement } from "../../core/helpers/createElement.mjs";
 import { Atom } from "../../core/Atom.mjs";
 
 export function Atom_ListItem2(data = "list data placeholder") {
   Atom.call(this);
 
   const component = async () => {
-    const comp = await html2dom`
-      <li class="${await this.atom()}">${data}</li>`;
+    // const comp = await html2dom`
+    //   <li class="${await this.atom()}">${data}</li>`;
+    // return comp;
+
+    const comp = await createElement("li", { class: await this.atom() }, data);
     return comp;
   };
 
---
+++ core/helpers/createElement.mjs
@@ -0,0 +1,26 @@
+export function createElement(tag, attributes = {}, ...children) {
+  const element = document.createElement(tag);
+
+  for (const [key, value] of Object.entries(attributes)) {
+    if (key === "className") {
+      const classes = value.split(" ");
+      for (const className of classes) {
+        element.classList.add(className);
+      }
+    } else if (key === "id") {
+      element.id = value;
+    } else {
+      element.setAttribute(key, value);
+    }
+  }
+
+  for (const child of children) {
+    if (typeof child === "string") {
+      element.appendChild(document.createTextNode(child));
+    } else {
+      element.appendChild(child);
+    }
+  }
+
+  return element;
+}
