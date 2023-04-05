91e5ac7f5d9d589929c2944f69fdfe9746549187
Maja Frankiewicz <majafrank@live.se>
2023-04-05 14:32:16 +0200

    auto commit

--- components/organisms/Organism_ListAllWIcons.mjs
+++ components/organisms/Organism_ListAllWIcons.mjs
@@ -44,9 +44,9 @@ export function Organism_ListAllWIcons() {
 
   this.getHtml = function () {
     return `
-      <div class="organism_list-all-search">
+      <div class="organism_list-all-search-icon">
         ${slot(this.molecules[0].molecule)}
-        <div id="organism_all_lists" class="organism_list-all-search__lists">
+        <div id="organism_all_lists" class="organism_list-all-search__lists-icon">
           ${this.molecules.slice(1).map((mol) => slot(mol.molecule)).join("")}
         </div>
         <div id="modal-slot">
