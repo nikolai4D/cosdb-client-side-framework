91ed9ff57c05f986c8629e55847e221aa58ccc6e
Maja Frankiewicz <majafrank@live.se>
2023-04-05 13:40:26 +0200

    auto commit

--- data-mgmt/etl/transformer_groupByFirstLetter.mjs
+++ data-mgmt/etl/transformer_groupByFirstLetter.mjs
@@ -8,27 +8,8 @@ export async function transformer_groupByFirstLetter(theArray) {
           acc[firstLetter].push(str);
           return acc;
         }, {});
-        console.log(grouped)
 
-        console.log(JSON.stringify(grouped))
-
-        // const flattenedGrouped = Object.values(grouped).flat();
-
-        // const sortedGrouped = flattenedGrouped
-        //   .sort((a, b) => a.title.localeCompare(b.title, "sv"));
-
-        // return sortedGrouped;
-
-
-      
-        // const sortedGrouped = Object.entries(grouped)
-        //   .sort(([a], [b]) => a.localeCompare(b, "sv"))
-        //   .map(([letter, title]) => ({ letter, title }));
-        
-
-        // return sortedGrouped;
-
-          const sortedGrouped = {};
+        const sortedGrouped = {};
 
         for (const [key, items] of Object.entries(grouped)) {
           sortedGrouped[key] = items.sort((a, b) => a.title.localeCompare(b.title, "sv"));
@@ -40,11 +21,3 @@ export async function transformer_groupByFirstLetter(theArray) {
   
 }
 
-// const grouped = theArray.reduce((acc, str) => {
-//   const firstLetter = str.title[0].toUpperCase();
-//   if (!acc[firstLetter]) {
-//     acc[firstLetter] = [];
-//   }
-//   acc[firstLetter].push(str);
-//   return acc;
-// }, {});
