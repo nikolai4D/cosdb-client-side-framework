351fc2c373874fc0be3e9eb774f2d2896ec527fb
Maja Frankiewicz <majafrank@live.se>
2023-04-05 13:18:55 +0200

    auto commit

--- data-mgmt/etl/transfomer_trimAllElements.mjs
+++ data-mgmt/etl/transfomer_trimAllElements.mjs
@@ -1,7 +1,5 @@
 export function transformer_trimAllElements(data, aKey) {
     return data.map((item) => {
-        item[aKey].trim()
-        console.log(item[aKey].trim())
-        return item
+        return {...item, [aKey]: item[aKey].trim()}
       })
 }
