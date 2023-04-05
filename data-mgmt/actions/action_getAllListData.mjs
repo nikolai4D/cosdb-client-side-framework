ef3604ad1a9b6a8040dee678e812a7b080900586
Maja Frankiewicz <majafrank@live.se>
2023-04-05 11:34:29 +0200

    auto commit

--- data-mgmt/actions/action_getAllListData.mjs
+++ data-mgmt/actions/action_getAllListData.mjs
@@ -2,6 +2,8 @@ import { apiCallPost } from './apiCalls.mjs'
 import { mutation_setAllListData } from '../mutations/mutation_setAllListData.mjs';
 
 export async function action_getAllListData({type, url}) {
+
+      console.log(url)
       const data =  await apiCallPost({url: "api/getListData", body: url});
       console.log(data)
       mutation_setAllListData({type, data})
--- data-mgmt/actions/action_getAllListDataFromArray.mjs
+++ data-mgmt/actions/action_getAllListDataFromArray.mjs
@@ -3,9 +3,9 @@ import { mutation_setAllListData } from '../mutations/mutation_setAllListData.mj
 
 export async function action_getAllListDataFromArray({type, urls}) {
 
-    for (const url of urls)
-    {
-      const data =  await apiCallPost({url: "api/getListData", body: url});
+    let data = []
+    for (const url of urls){
+      data.push(...await apiCallPost({url: "api/getListData", body: url}))
     }
       mutation_setAllListData({type, data})
 }
