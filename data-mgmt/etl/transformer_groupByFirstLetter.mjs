bd06b26188e0ca90a732b780880a493034bc0fa8
Maja Frankiewicz <majafrank@live.se>
2023-04-05 13:37:48 +0200

    auto commit

--- data-mgmt/etl/transformer_groupByFirstLetter.mjs
+++ data-mgmt/etl/transformer_groupByFirstLetter.mjs
@@ -9,17 +9,42 @@ export async function transformer_groupByFirstLetter(theArray) {
           return acc;
         }, {});
         console.log(grouped)
+
+        console.log(JSON.stringify(grouped))
+
+        // const flattenedGrouped = Object.values(grouped).flat();
+
+        // const sortedGrouped = flattenedGrouped
+        //   .sort((a, b) => a.title.localeCompare(b.title, "sv"));
+
+        // return sortedGrouped;
+
+
       
-        const flattenedGrouped = Object.values(grouped).flat();
-
-        const sortedGrouped = flattenedGrouped
-          .sort((a, b) => a.title.localeCompare(b.title, "sv"));
+        // const sortedGrouped = Object.entries(grouped)
+        //   .sort(([a], [b]) => a.localeCompare(b, "sv"))
+        //   .map(([letter, title]) => ({ letter, title }));
         
-          console.log(sortedGrouped)
 
+        // return sortedGrouped;
+
+          const sortedGrouped = {};
+
+        for (const [key, items] of Object.entries(grouped)) {
+          sortedGrouped[key] = items.sort((a, b) => a.title.localeCompare(b.title, "sv"));
+        }
+
+        console.log(sortedGrouped);
 
         return sortedGrouped;
-        
   
 }
 
+const grouped = theArray.reduce((acc, str) => {
+  const firstLetter = str.title[0].toUpperCase();
+  if (!acc[firstLetter]) {
+    acc[firstLetter] = [];
+  }
+  acc[firstLetter].push(str);
+  return acc;
+}, {});
