db10e78409f28887187a7c9b4d9b380904285795
Maja Frankiewicz <majafrank@live.se>
2023-04-06 08:27:23 +0200

    auto commit

--- components/organisms/Organism_ListAllWIcons.mjs
+++ components/organisms/Organism_ListAllWIcons.mjs
@@ -144,7 +144,7 @@ export function Organism_ListAllWIcons() {
       });
   
     });
-    this.molecules= [...newMolecules]
+    this.molecules= [newMolecules]
     console.log(newMolecules)
   };
 
