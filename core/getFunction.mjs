6adce9bc37964ed5b0b5d96e9e92363079e70853
Nicholas Alvén <nicholas@nikolaidev.com>
2023-04-07 11:28:29 +0200

    auto commit

--- core/getFunction.mjs
+++ core/getFunction.mjs
@@ -3,12 +3,15 @@ import { apiCallGet } from "../data-mgmt/actions/apiCalls.mjs";
 import { createAction } from "./helpers/createAction.mjs";
 
 export async function getFunction(module, parentId) {
+  console.log("getFunction", module, parentId);
   const modelFunction = await apiCallGet(`/read/functions`);
+  console.log("modelFunction", modelFunction);
 
   const func = modelFunction.filter((fn) => fn.parentId === parentId);
   const parameters = func.parameters;
 
   const funcObject = await createAction(parameters, module);
+  console.log(funcObject);
 
   return funcObject;
 }
--- core/helpers/createAction.mjs
+++ core/helpers/createAction.mjs
@@ -2,9 +2,12 @@ import { importModuleFromFile } from "./importModuleFromFile.mjs";
 
 export async function createAction(parameters = null, file) {
   const pathToAction = `../../../data-mgmt/actions/${file}.mjs`;
+  console.log("pathToAction", pathToAction);
   const Action = await importModuleFromFile(pathToAction, file);
+  console.log("Action", Action);
 
   const action = new Action[file](parameters);
+  console.log("action", action);
 
   return action;
 }
