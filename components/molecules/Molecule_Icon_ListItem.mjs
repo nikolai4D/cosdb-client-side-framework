65be4ec60e8cb8b882f374ca58fb5e6033972e04
Nicholas Alvén <nicholas@nikolaidev.com>
2023-04-07 11:19:52 +0200

    auto commit

--- components/molecules/Molecule_Icon_ListItem.mjs
+++ components/molecules/Molecule_Icon_ListItem.mjs
@@ -31,16 +31,12 @@ export function Molecule_Icon_ListItem() {
 
   //build component
   const component = async () => {
+    console.log(await listItems());
     const comp = await createElement(
       "div",
       { className: "Molecule_Template" },
-      await createElement(
-        "ul",
-        {},
-        ...(await listItemsWithIcons(await listItems()))
-      )
+      await createElement("ul", {}, null)
     );
-
     //add event listener to the comp here
 
     return comp;
@@ -57,16 +53,16 @@ export function Molecule_Icon_ListItem() {
   const listItems = async () => await this.fn(1);
 
   //loopa varje item och sätt in i Atom_ListItem
-  const listItemsWithIcons = async (arrayOfData) => {
-    return await Promise.all(
-      arrayOfData.map(async (item) => {
-        return await createElement(
-          "div",
-          {},
-          await this.atom(1, item.icon),
-          await this.atom(2, item.title)
-        );
-      })
-    );
-  };
+  //   const listItemsWithIcons = async (arrayOfData) => {
+  //     return await Promise.all(
+  //       arrayOfData.map(async (item) => {
+  //         return await createElement(
+  //           "div",
+  //           {},
+  //           await this.atom(1, item.icon),
+  //           await this.atom(2, item.title)
+  //         );
+  //       })
+  //     );
+  //   };
 }
