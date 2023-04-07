af24fa6da730b0e9989cd284c1be9d4241ce1b2c
Nicholas Alvén <nicholas@nikolaidev.com>
2023-04-07 08:42:11 +0200

    auto commit

--- core/GetComponent.mjs
+++ core/GetComponent.mjs
@@ -4,9 +4,6 @@ import { getMolecule } from "./getMolecule.mjs";
 import { getAtom } from "./getAtom.mjs";
 
 export async function getComponent(compName, compParentId) {
-  //   const div = document.createElement("div");
-  //   div.classList.add("component");
-  //   div.setAttribute("id", compParentId);
   let comp;
 
   if (compName.startsWith("Organism")) {
@@ -35,12 +32,7 @@ export async function getComponent(compName, compParentId) {
     comp = getComp.comp;
     comp.value = [{ value: compValue[0].value }];
   }
-  console.log(comp);
   const renderComp = await comp.render();
-
-  //   div.appendChild(renderComp);
-
-  //   return div;
 
   const component = await createElement(
     "div",
