0e8dadddbc91ac92fbeff1d40a1da442eeb2ec07
Nicholas Alvén <nicholas@nikolaidev.com>
2023-04-07 11:30:29 +0200

    auto commit

--- core/helpers/createAction.mjs
+++ core/helpers/createAction.mjs
@@ -1,12 +1,13 @@
 import { importModuleFromFile } from "./importModuleFromFile.mjs";
 
 export async function createAction(parameters = null, file) {
+  console.log("createAction", parameters, file);
   const pathToAction = `../../../data-mgmt/actions/${file}.mjs`;
   console.log("pathToAction", pathToAction);
   const Action = await importModuleFromFile(pathToAction, file);
   console.log("Action", Action);
 
-  const action = new Action[file](parameters);
+  const action = new Action[file]();
   console.log("action", action);
 
   return action;
