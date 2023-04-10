1e83ae9bdd98b2dddbf702a7af3bdf362ce1e6d8
Nicholas Alvén <nicholas@nikolaidev.com>
2023-04-10 10:50:46 +0200

    auto commit

--- data-mgmt/mutations/mutation_setAllListData.mjs
+++ data-mgmt/mutations/mutation_setAllListData.mjs
@@ -1,5 +1,7 @@
 import { State } from "../state.mjs";
 
 export async function mutation_setAllListData(type, sortedData) {
+  console.log("mutation_setAllListData: ", type, sortedData);
   State[type] = sortedData;
+  console.log("State.items: ", State.items);
 }
