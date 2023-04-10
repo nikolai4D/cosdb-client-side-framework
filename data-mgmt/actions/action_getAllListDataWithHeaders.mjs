da8177a82ff2d9efb3c98fb684044e158a1bab89
Nicholas Alvén <nicholas@nikolaidev.com>
2023-04-10 09:09:54 +0200

    auto commit

--- data-mgmt/actions/action_getAllListDataWithHeaders.mjs
+++ data-mgmt/actions/action_getAllListDataWithHeaders.mjs
@@ -5,6 +5,7 @@ export async function action_getAllListDataWithHeaders({
   type = "type",
   parentIds = [],
 }) {
+  console.log(type, parentIds);
   //for each parent id, get the list data
 
   const listData = [];
