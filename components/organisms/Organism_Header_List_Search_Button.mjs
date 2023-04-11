47afd0d67579d8e5a903c668c5c315f6eb0363d9
Nicholas Alvén <nicholas@nikolaidev.com>
2023-04-11 03:10:16 +0200

    auto commit

--- components/organisms/Organism_Header_List_Search_Button.mjs
+++ components/organisms/Organism_Header_List_Search_Button.mjs
@@ -81,7 +81,7 @@ export function Organism_Header_List_Search_Button() {
       ...(await listItems(compData))
     );
     headingList.addEventListener("click", async (e) => {
-      if (e.target.tagName === "LI") {
+      if (e.target.tagName === "P") {
         console.log(e.target);
         // const modalContent = e.target.textContent;
         await openModal();
