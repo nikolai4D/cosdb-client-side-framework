22499e879892e5ed6ef7edfcaca560e94473d4b9
Nicholas Alvén <nicholas@nikolaidev.com>
2023-04-14 18:01:29 +0200

    auto commit

--- components/molecules/Molecule_Icon_NavItem.mjs
+++ components/molecules/Molecule_Icon_NavItem.mjs
@@ -58,7 +58,6 @@ export function Molecule_Icon_NavItem() {
 
   //get menu items from await this.fn(1)
   const navItems = async () => {
-    console.log(await this.fn(1));
     return (
       (await this.fn(1)) || [{ icon: "icon", title: "title", route: "route" }]
     );
