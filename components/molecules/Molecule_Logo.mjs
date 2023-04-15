cb9faa643190c140b794ceeeb3cfbcad2d13a9c0
Nicholas Alvén <nicholas@nikolaidev.com>
2023-04-15 16:53:31 +0200

    auto commit

--- components/molecules/Molecule_Logo.mjs
+++ components/molecules/Molecule_Logo.mjs
@@ -3,7 +3,7 @@ import { Molecule } from "../../core/Molecule.mjs";
 import { createElement } from "../../core/helpers/createElement.mjs";
 //import sub components
 import { Atom_Icon } from "../atoms/Atom_Icon.mjs";
-import { Atom_Heading4 } from "../atoms/Atom_Heading4.mjs";
+import { Atom_Heading4value } from "../atoms/Atom_Heading4value.mjs";
 
 export function Molecule_Logo() {
   Molecule.call(this);
@@ -17,8 +17,8 @@ export function Molecule_Logo() {
     },
     {
       id: 2,
-      atom: "Atom_Heading4",
-      component: new Atom_Heading4(),
+      atom: "Atom_Heading4value",
+      component: new Atom_Heading4value(),
     },
   ];
 
@@ -36,8 +36,8 @@ export function Molecule_Logo() {
     const comp = await createElement(
       "div",
       { className: "molecule_logo" },
-      await this.atom(1, "icon"),
-      await this.atom(2, "Urban Cloud")
+      await this.atom(1),
+      await this.atom(2)
     );
     //add event listener to the comp here
 
