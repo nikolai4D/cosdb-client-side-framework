4feff7843511c0bd1bf7406d4d2106fcf87e34b6
Nicholas Alvén <nicholas@nikolaidev.com>
2023-04-24 22:27:31 +0200

    auto commit

--- core/Router.mjs
+++ core/Router.mjs
@@ -1,12 +1,29 @@
+// import { View } from "./View.mjs";
+
+// export async function Router(viewPath) {
+//   await View(viewPath);
+// }
+// // Listen for back or forward navigation
+// window.addEventListener("popstate", async (event) => {
+//   if (event.state && event.state.viewPath) {
+//     const viewPath = event.state.viewPath;
+//     await View(viewPath);
+//   }
+// });
+
 import { View } from "./View.mjs";
 
 export async function Router(viewPath) {
   await View(viewPath);
 }
+
 // Listen for back or forward navigation
 window.addEventListener("popstate", async (event) => {
+  console.log("popstate event triggered:", event);
+
   if (event.state && event.state.viewPath) {
     const viewPath = event.state.viewPath;
+    console.log("Calling View with viewPath:", viewPath);
     await View(viewPath);
   }
 });
