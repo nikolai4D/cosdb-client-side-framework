b1206ae59bd7092f8815ff668c9cfceed995dd5a
Nicholas Alvén <nicholas@nikolaidev.com>
2023-04-24 16:29:01 +0200

    auto commit

--- core/getComponents.mjs
+++ core/getComponents.mjs
@@ -9,18 +9,16 @@ export async function getComponents(parentId) {
   //const Slots = await apiCallGet(`/read/${slotType}s/${parentId}`);
   const Slots = State.components.slots.filter((s) => s.parentId === parentId);
   const componentsArray = [];
-  console.log("Slots", Slots);
 
   if (Slots.length > 0) {
     for (const slot of Slots) {
       let comp = null;
       try {
         //comp = await apiCallGet(`/read/${componentType}s/${slot.id}`);
-        console.log("State.components.components", State.components.components);
+
         comp = State.components.components.filter(
           (c) => c.parentId === slot.id
         );
-        console.log("comp", comp);
       } catch (error) {
         console.log("no component found for slot ", slot.value);
       }
