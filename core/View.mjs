766c478f9fff3c546f7830433d9324fb2eb1d4b2
Nicholas Alvén <nicholas@nikolaidev.com>
2023-04-24 18:40:13 +0200

    auto commit

--- core/View.mjs
+++ core/View.mjs
@@ -1,3 +1,53 @@
+// import { apiCallGet } from "../data-mgmt/actions/apiCalls.mjs";
+// import { getViewTemplate } from "./getViewTemplate.mjs";
+// import { createElement } from "./helpers/createElement.mjs";
+// import { State } from "../data-mgmt/State.mjs";
+
+// export async function View(viewPath) {
+//   const type = "view";
+
+//   //validate and authenticate path
+//   const newView = await apiCallGet(`/auth/${viewPath}`);
+//   const id = newView.id;
+//   const value = newView.value;
+
+//   //set components to state
+//   if (!State.components) {
+//     State.components = {};
+//     State.components.viewTemplates = await apiCallGet(`/read/viewTemplates`);
+//     State.components.slots = await apiCallGet(`/read/slots`);
+//     State.components.components = await apiCallGet(`/read/components`);
+//     State.components.organisms = await apiCallGet(`/read/organisms`);
+//     State.components.molecules = await apiCallGet(`/read/molecules`);
+//     State.components.atoms = await apiCallGet(`/read/atoms`);
+//     State.components.atomValues = await apiCallGet(`/read/atomValues`);
+//     State.components.functions = await apiCallGet(`/read/functions`);
+//   }
+
+//   //set browser history
+//   window.history.pushState({ viewPath: value }, "", value);
+
+//   //delete previous view
+//   await deletePreviousView();
+
+//   // Create a new viewTemplate
+//   const divChild = await getViewTemplate(id);
+
+//   // Create a new div from type
+//   const div = await createElement("div", { class: type, id: id }, divChild);
+
+//   // Append the view to the body
+//   document.body.appendChild(div);
+// }
+
+// function deletePreviousView() {
+//   const previousDiv = document.querySelector(".view");
+
+//   if (previousDiv) {
+//     previousDiv.remove();
+//   }
+// }
+
 import { apiCallGet } from "../data-mgmt/actions/apiCalls.mjs";
 import { getViewTemplate } from "./getViewTemplate.mjs";
 import { createElement } from "./helpers/createElement.mjs";
@@ -6,12 +56,12 @@ import { State } from "../data-mgmt/State.mjs";
 export async function View(viewPath) {
   const type = "view";
 
-  //validate and authenticate path
+  // validate and authenticate path
   const newView = await apiCallGet(`/auth/${viewPath}`);
   const id = newView.id;
   const value = newView.value;
 
-  //set components to state
+  // set components to state
   if (!State.components) {
     State.components = {};
     State.components.viewTemplates = await apiCallGet(`/read/viewTemplates`);
@@ -24,27 +74,42 @@ export async function View(viewPath) {
     State.components.functions = await apiCallGet(`/read/functions`);
   }
 
-  //set browser history
+  // set browser history
   window.history.pushState({ viewPath: value }, "", value);
 
-  //delete previous view
+  // delete previous view
   await deletePreviousView();
 
-  //   // Create a new div from type
-  //   const div = document.createElement("div");
-  //   div.classList.add(type);
-  //   div.setAttribute("id", id);
-
   // Create a new viewTemplate
-  const divChild = await getViewTemplate(id);
-
-  //   div.appendChild(divChild);
+  const newViewTemplate = await getViewTemplate(id);
 
   // Create a new div from type
-  const div = await createElement("div", { class: type, id: id }, divChild);
+  const newDiv = await createElement(
+    "div",
+    { class: type, id: id },
+    newViewTemplate
+  );
 
-  // Append the view to the body
-  document.body.appendChild(div);
+  // get existing view div
+  const existingDiv = document.querySelector(".view");
+
+  // append only new or updated elements
+  if (!existingDiv) {
+    // no existing view, append the new div to the body
+    document.body.appendChild(newDiv);
+  } else {
+    // compare the two DOM structures
+    const newChildren = Array.from(newDiv.children);
+    const existingChildren = Array.from(existingDiv.children);
+
+    // append only new or updated elements
+    newChildren.forEach((child) => {
+      const matchingChild = existingChildren.find((c) => c.isEqualNode(child));
+      if (!matchingChild) {
+        existingDiv.appendChild(child);
+      }
+    });
+  }
 }
 
 function deletePreviousView() {
