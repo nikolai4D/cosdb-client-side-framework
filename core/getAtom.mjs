bd0c92d736d78133447543c8e372a968e162cb72
Nicholas Alvén <nicholas@nikolaidev.com>
2023-04-24 15:39:17 +0200

    auto commit

--- core/getAtom.mjs
+++ core/getAtom.mjs
@@ -6,16 +6,18 @@ export async function getAtom(module, parentId, atomId = null) {
   const type = "atom";
   //const modelAtoms = await apiCallGet(`/read/atoms`);
   const modelAtoms = State.components.atoms;
+  console.log(modelAtoms);
   //const modelAtomValues = await apiCallGet(`/read/atomValues`);
   const modelAtomValues = State.components.atomValues;
+  console.log(modelAtomValues);
 
   const atom = atomId
     ? modelAtoms.filter((atom) => atom.id === atomId)
     : modelAtoms.filter((atom) => atom.parentId === parentId);
 
-  const atomValue = modelAtomValues.filter(
-    (atomValue) => atomValue.parentId === atom[0].id
-  );
+  console.log(atomId);
+
+  const atomValue = modelAtomValues.filter((at) => at.parentId === atom[0].id);
 
   const atomObject = await createComponent(type, module);
 
