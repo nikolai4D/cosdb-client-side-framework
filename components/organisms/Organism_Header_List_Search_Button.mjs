f7f290fa74231c68818f0ceac4fa086d0f3833fc
Nicholas Alvén <nicholas@nikolaidev.com>
2023-05-04 13:31:20 +0200

    auto commit

--- components/organisms/Organism_Header_List_Search_Button.mjs
+++ components/organisms/Organism_Header_List_Search_Button.mjs
@@ -194,7 +194,7 @@ export function Organism_Header_List_Search_Button() {
         },
         await this.childOrganism(2, e.target.id)
       );
-      existingModal.insertBefore(newModalContent, existingModal.firstChild);
+      existingModal.appendChild(newModalContent);
     }
 
     if (e.target === existingModal || e.target === modalContentBackButton) {
