967f559d86160a49d5989cc82be449e5328adfd2
Maja Frankiewicz <majafrank@live.se>
2023-06-01 16:03:07 +0200

    auto commit

--- cms/cms-app/functions/updateModelIfHasChanged.mjs
+++ cms/cms-app/functions/updateModelIfHasChanged.mjs
@@ -1,5 +1,33 @@
+import { action_readModel } from "../data-mgmt/actions/action_readModel.mjs";
 
 export async function updateModelIfHasChanged() {
 
+    const readModel = await action_readModel();
+    console.log(readModel)
+
     console.log("Hello!")
+
+    // get the code structure
+        // does the viewtemplate still exist?*
+            // are the slots still the same?*
+
+            // the component in the slot, 
+                // does the component still exist?*
+
+                    // if it's an organism,*
+                        // are the organisms still the same?*
+                        // are the molecules still the same?*
+                        // are the atoms still the same?*
+
+                    // if it's a molecule,*
+                        // are the atoms still the same?*
+
+                // are the functions still the same?*
+
+ // * if not, then remove and replace with new component
+ 
+                // Read Model 
+    // Change model
+
+
 }
