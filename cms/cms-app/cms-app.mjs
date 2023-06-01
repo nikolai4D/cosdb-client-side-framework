af47f78b19ce96eb407beab688e7df52d4819b6c
Maja Frankiewicz <majafrank@live.se>
2023-06-01 15:06:11 +0200

    auto commit

--- cms/cms-app/cms-app.mjs
+++ cms/cms-app/cms-app.mjs
@@ -1,7 +1,9 @@
-import { createViewButton } from "./functions/createViewButton.mjs";
+import { updateModelIfHasChanged } from "./functions/updateModelIfHasChanged.mjs";
 import { readExistingModel } from "./functions/readExistingModel.mjs";
+import { createViewButton } from "./functions/createViewButton.mjs";
 
 export async function cms_app() {
+  await updateModelIfHasChanged();
   await readExistingModel();
   await createViewButton();
 }
