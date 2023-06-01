550d7a823125d8135f59b3f976152255979db937
Maja Frankiewicz <majafrank@live.se>
2023-06-01 17:14:00 +0200

    auto commit

--- components/viewTemplates/ViewTemplate_Test1.mjs
+++ components/viewTemplates/ViewTemplate_Test1.mjs
@@ -2,7 +2,7 @@
 import { createElement } from "../../core/helpers/createElement.mjs";
 import { ViewTemplate } from "../../core/ViewTemplate.mjs";
 
-export function ViewTemplate_Test() {
+export function ViewTemplate_Test1() {
   ViewTemplate.call(this);
 
   // sub components
