4f158e3c6cba1553ef0ceaa4b048e07bf77a58c6
Maja Frankiewicz <majafrank@live.se>
2023-06-02 15:32:54 +0200

    auto commit

--- cms/cms-app/functions/getConstructors.mjs
+++ cms/cms-app/functions/getConstructors.mjs
@@ -3,6 +3,7 @@ import { importModuleFromFile } from "./importModuleFromFile.mjs";
 export async function getConstructors(filename, constructorType, type) {
   const file = `${filename}.mjs`;
   const module = await importModuleFromFile(file, filename, type);
+  console.log("HELLLOOOO",module);
   const constructors = module[constructorType];
 
   return constructors;
