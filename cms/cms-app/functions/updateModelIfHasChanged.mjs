962488adbad97307d6cf16b67bbf788fa5392d4e
Maja Frankiewicz <majafrank@live.se>
2023-06-05 11:28:14 +0200

    auto commit

--- cms/cms-app/functions/updateModelIfHasChanged.mjs
+++ cms/cms-app/functions/updateModelIfHasChanged.mjs
@@ -224,14 +224,16 @@ async function checkOrganismSubComponents(organismInState, componentFiles){
 }
 
 async function checkSubComponents(subComponentsInState, subComponentFile, componentFiles, type) {
-    for (let subComponent of subComponentsInState) {
-        checkIfSubcomponentStateMatchInFile(subComponent, subComponentFile, type);
-    }
+    if (type !== 'atom') {
 
-    for (let subComponent of subComponentFile[type + "s"]) {
-        checkIfSubcomponentFileMatchState(subComponent, subComponentsInState, type);
-    }
+        for (let subComponent of subComponentsInState) {
+            checkIfSubcomponentStateMatchInFile(subComponent, subComponentFile, type);
+        }
 
+        for (let subComponent of subComponentFile[type + "s"]) {
+            checkIfSubcomponentFileMatchState(subComponent, subComponentsInState, type);
+        }
+    }
     for (let subComponent of subComponentsInState) {
         if (type === 'organism') {
             await checkOrganismSubComponents(subComponent, componentFiles);
