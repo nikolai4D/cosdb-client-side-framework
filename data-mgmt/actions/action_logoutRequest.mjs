78f841e10c9f9c6c9fc6124b35b9743efa14bc6c
Maja Frankiewicz <majafrank@live.se>
2023-06-11 14:31:13 +0200

    auto commit

---
+++ data-mgmt/actions/action_logoutRequest.mjs
@@ -0,0 +1,16 @@
+import { action_routeToView } from "./action_routeToView.mjs";
+
+export async function action_loginRequest() {
+    //Clear sessionStorage (accessToken) and set jwt cookie (refreshToken) to expire in the past
+    sessionStorage.clear();
+    await fetch("/logout");
+
+    //Clear State
+    for (const key in State) {
+        delete State[key]
+    }
+    State.accessToken = undefined;
+
+    //Redirection Part
+    await navigateTo("/login")
+}
