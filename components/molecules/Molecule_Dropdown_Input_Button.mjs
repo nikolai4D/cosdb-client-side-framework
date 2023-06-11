22e6eca714d9f9f4e2aafc996a13ef685cea797e
Nicholas Alvén <nicholas@nikolaidev.com>
2023-06-11 16:07:08 +0200

    auto commit

--- components/molecules/Molecule_Dropdown_Input_Button.mjs
+++ components/molecules/Molecule_Dropdown_Input_Button.mjs
@@ -38,8 +38,8 @@ export function Molecule_Dropdown_Input_Button() {
       "div",
       { class: "molecule_dropdown_input_button" },
      await this.atom(1, compData),
-      await this.atom(2, null),
-     await this.atom(3, "inputFieldNewObject")
+      await this.atom(2, "inputFieldNewObject"),
+     await this.atom(3, null)
     );
 
     //add event listener to the comp here
